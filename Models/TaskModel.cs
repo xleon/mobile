46bb5378fcda47b9e900e982e59bf6380fa2a5ce
Mart Roosmaa <roosmaa@gmail.com>
2014-01-07 10:54:54 +0200

    Added data fields to TaskModel.

--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -7,5 +7,90 @@ namespace TogglDoodle.Models
         public static long NextId {
             get { return Model.NextId<TaskModel> (); }
         }
+
+        private readonly int workspaceRelationId;
+        private readonly int projectRelationId;
+
+        public TaskModel ()
+        {
+            workspaceRelationId = ForeignRelation (() => WorkspaceId, () => Workspace);
+            projectRelationId = ForeignRelation (() => ProjectId, () => Project);
+        }
+
+        #region Data
+
+        private string name;
+
+        public string Name {
+            get { return name; }
+            set {
+                if (name == value)
+                    return;
+
+                ChangePropertyAndNotify (() => Name, delegate {
+                    name = value;
+                });
+            }
+        }
+
+        private bool active;
+
+        public bool IsActive {
+            get { return active; }
+            set {
+                if (active == value)
+                    return;
+
+                ChangePropertyAndNotify (() => IsActive, delegate {
+                    active = value;
+                });
+            }
+        }
+
+        private long estimate;
+
+        public long Estimate {
+            get { return estimate; }
+            set {
+                if (estimate == value)
+                    return;
+
+                ChangePropertyAndNotify (() => Estimate, delegate {
+                    estimate = value;
+                });
+            }
+        }
+
+        #endregion
+
+        #region Relations
+
+        public long? WorkspaceId {
+            get { return GetForeignId (workspaceRelationId); }
+            set { SetForeignId (workspaceRelationId, value); }
+        }
+
+        [DontDirty]
+        [SQLite.Ignore]
+        public WorkspaceModel Workspace {
+            get { return GetForeignModel<WorkspaceModel> (workspaceRelationId); }
+            set { SetForeignModel (workspaceRelationId, value); }
+        }
+
+        public long? ProjectId {
+            get { return GetForeignId (projectRelationId); }
+            set { SetForeignId (projectRelationId, value); }
+        }
+
+        [DontDirty]
+        [SQLite.Ignore]
+        public ProjectModel Project {
+            get { return GetForeignModel<ProjectModel> (projectRelationId); }
+            set { SetForeignModel (projectRelationId, value); }
+        }
+
+        #endregion
+
+        // TODO: Reverse relation for tasks
     }
 }

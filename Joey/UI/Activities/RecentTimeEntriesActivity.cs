a624fd76c3237ed0063492ca302e4d56af1abdff
Mart Roosmaa <mart@toggl.com>
2014-01-21 13:26:16 +0200

    Add RecentTimeEntriesActivity placeholder.

---
+++ Joey/UI/Activities/RecentTimeEntriesActivity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using Android.App;
+using Android.OS;
+
+namespace Toggl.Joey.UI.Activities
+{
+    [Activity (
+        Label = "@string/EntryName",
+        MainLauncher = true)]
+    public class RecentTimeEntriesActivity : BaseActivity
+    {
+        protected override void OnCreate (Bundle bundle)
+        {
+            base.OnCreate (bundle);
+        }
+    }
+}

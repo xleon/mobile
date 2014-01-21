e8edce00fb19423060d842f5bc9ad0e6e4c2626b
Mart Roosmaa <mart@toggl.com>
2014-01-21 13:10:28 +0200

    Add BaseActivity for Android project.
    Add a common ancestor for all activities. Implements authentication
    check and force-redirects to LoginActivity.

---
+++ Joey/UI/Activities/BaseActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Android.App;
+using XPlatUtils;
+using Toggl.Phoebe.Net;
+using Android.Content;
+
+namespace Toggl.Joey.UI.Activities
+{
+    public abstract class BaseActivity : Activity
+    {
+        protected virtual bool RequireAuth {
+            get { return true; }
+        }
+
+        private void CheckAuth ()
+        {
+            if (!RequireAuth)
+                return;
+            var authManager = ServiceContainer.Resolve<AuthManager> ();
+            if (!authManager.IsAuthenticated) {
+                var intent = new Intent (this, typeof(LoginActivity));
+                intent.AddFlags (ActivityFlags.ClearTop);
+                StartActivity (intent);
+                Finish ();
+            }
+        }
+
+        protected override void OnCreate (Android.OS.Bundle state)
+        {
+            base.OnCreate (state);
+            CheckAuth ();
+        }
+
+        protected override void OnResume ()
+        {
+            base.OnResume ();
+            CheckAuth ();
+        }
+    }
+}

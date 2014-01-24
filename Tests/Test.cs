48dbe4b087d0ae0408c137bd711d95f1d0d6d615
Mart Roosmaa <mart@toggl.com>
2014-01-24 23:36:18 +0200

    Add tests project skeleton.

---
+++ Tests/Test.cs
@@ -0,0 +1,14 @@
+using System;
+using NUnit.Framework;
+
+namespace Toggl.Phoebe.Tests
+{
+    [TestFixture ()]
+    public class Test
+    {
+        [Test ()]
+        public void TestCase ()
+        {
+        }
+    }
+}

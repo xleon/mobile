37f71675670a73a026eced843d6c57e90556aeae
Mart Roosmaa <mart@toggl.com>
2014-01-25 12:56:13 +0200

    Test promoting model to shared state.

--- Tests/Data/ModelTest.cs
+++ Tests/Data/ModelTest.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Linq;
 using NUnit.Framework;
 using Toggl.Phoebe.Data;
+using XPlatUtils;
 
 namespace Toggl.Phoebe.Tests.Data
 {
@@ -9,6 +11,30 @@ namespace Toggl.Phoebe.Tests.Data
     {
         private class PlainModel : Model
         {
+        }
+
+        [TestFixtureSetUp]
+        public void Init ()
+        {
+            ServiceContainer.Register<MessageBus> ();
+        }
+
+        [TestFixtureTearDown]
+        public void Cleanup ()
+        {
+            ServiceContainer.Clear ();
+        }
+
+        [SetUp]
+        public void SetUp ()
+        {
+            ServiceContainer.AddScope ();
+        }
+
+        [TearDown]
+        public void TearDown ()
+        {
+            ServiceContainer.RemoveScope ();
         }
 
         [Test]
@@ -27,6 +53,38 @@ namespace Toggl.Phoebe.Tests.Data
             Assert.IsNull (model.RemoteDeletedAt, "RemoteDeletedAt must be null");
             Assert.IsEmpty (model.Errors, "Errors must be empty");
             Assert.IsTrue (model.IsValid, "IsValid must be true");
+        }
+
+        private MessageBus MessageBus {
+            get { return ServiceContainer.Resolve<MessageBus> (); }
+        }
+
+        [Test]
+        public void TestMakeShared ()
+        {
+            // Verify the ModelChangedMessage send count
+            var messageCount = 0;
+            MessageBus.Subscribe<ModelChangedMessage> ((msg) => {
+                messageCount++;
+            });
+
+            var model = new PlainModel ();
+            model.PropertyChanged += (sender, e) => {
+                if (e.PropertyName == PlainModel.PropertyIsShared) {
+                    // Check that model is present in cache
+                    Assert.That (Model.GetCached<PlainModel> (), Has.Exactly (1).SameAs (model), "The newly shared object should be present in cache already.");
+                } else if (e.PropertyName == PlainModel.PropertyId) {
+                    // Expect ID assignment
+                } else {
+                    Assert.Fail (String.Format ("Property '{0}' changed unexpectedly.", e.PropertyName));
+                }
+            };
+
+            var shared = Model.Update (model);
+
+            Assert.AreSame (model, shared, "Promoting to shared should return the initial model.");
+            Assert.NotNull (model.Id, "Should have received a new unique Id.");
+            Assert.AreEqual (messageCount, 1, "Received invalid number of OnModelChanged messages");
         }
     }
 }

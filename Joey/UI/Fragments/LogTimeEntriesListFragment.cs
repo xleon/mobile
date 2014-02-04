77c34d3a1324227b54e8fc9b1d51dad7182f6bdb
Mart Roosmaa <mart@toggl.com>
2014-02-04 17:10:34 +0200

    Add context action bar to log list view.
    The LogTimeEntriesListFragment now displays a context action bar when
    long pressing on the entries. The delete option is implemented, editing
    should navigate to currently not-existing edit time entry activity.

--- Joey/UI/Fragments/LogTimeEntriesListFragment.cs
+++ Joey/UI/Fragments/LogTimeEntriesListFragment.cs
@@ -1,20 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Android.App;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
+using Toggl.Phoebe.Data.Models;
 using Toggl.Joey.UI.Adapters;
 
 namespace Toggl.Joey.UI.Fragments
 {
-    public class LogTimeEntriesListFragment : ListFragment
+    public class LogTimeEntriesListFragment : ListFragment, AbsListView.IMultiChoiceModeListener
     {
         public override void OnViewCreated (View view, Bundle savedInstanceState)
         {
             base.OnViewCreated (view, savedInstanceState);
 
             ListAdapter = new LogTimeEntriesAdapter ();
+            ListView.ChoiceMode = (ChoiceMode)AbsListViewChoiceMode.MultipleModal;
+            ListView.SetMultiChoiceModeListener (this);
         }
 
         public override void OnListItemClick (ListView l, View v, int position, long id)
@@ -29,6 +33,70 @@ namespace Toggl.Joey.UI.Fragments
 
             model.IsPersisted = true;
             model.Continue ();
+        }
+
+        void AbsListView.IMultiChoiceModeListener.OnItemCheckedStateChanged (ActionMode mode, int position, long id, bool @checked)
+        {
+            int checkedCount = ListView.CheckedItemCount;
+            mode.Title = String.Format ("{0} selected", checkedCount);
+            mode.Menu.FindItem (Resource.Id.EditMenuItem).SetEnabled (checkedCount == 1);
+        }
+
+        bool ActionMode.ICallback.OnCreateActionMode (ActionMode mode, IMenu menu)
+        {
+            mode.MenuInflater.Inflate (Resource.Menu.LogTimeEntriesContextMenu, menu);
+            return true;
+        }
+
+        bool ActionMode.ICallback.OnPrepareActionMode (ActionMode mode, IMenu menu)
+        {
+            return false;
+        }
+
+        bool ActionMode.ICallback.OnActionItemClicked (ActionMode mode, IMenuItem item)
+        {
+            switch (item.ItemId) {
+            case Resource.Id.DeleteMenuItem:
+                DeleteCheckedTimeEntries ();
+                mode.Finish ();
+                return true;
+            case Resource.Id.EditMenuItem:
+                // TODO: Show time entry editing
+                return true;
+            default:
+                return false;
+            }
+        }
+
+        void ActionMode.ICallback.OnDestroyActionMode (ActionMode mode)
+        {
+        }
+
+        private void DeleteCheckedTimeEntries ()
+        {
+            var adapter = ListView.Adapter as LogTimeEntriesAdapter;
+            if (adapter == null)
+                return;
+
+            // Find models to delete:
+            var checkedPositions = ListView.CheckedItemPositions;
+            var arrSize = checkedPositions.Size ();
+            var toDelete = new List<TimeEntryModel> (arrSize);
+
+            for (var i = 0; i < arrSize; i++) {
+                var position = checkedPositions.KeyAt (i);
+                var isChecked = checkedPositions.Get (position);
+
+                var model = adapter.GetModel (position);
+                if (model != null)
+                    toDelete.Add (model);
+            }
+
+            // Delete models:
+            foreach (var model in toDelete) {
+                model.IsPersisted = true;
+                model.Delete ();
+            }
         }
     }
 }

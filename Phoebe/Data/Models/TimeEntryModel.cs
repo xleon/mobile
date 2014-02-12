xleon/mobile

commit 5da30ae229f229fc9929db230993d3a0505ef313

Fix continuing running time entry.
Take into account the the user might be trying to continue already
running time entry. There is no point in creating a duplicate of the
existing time entry, as it is done now.

Closes #30.

Phoebe/Data/Models/TimeEntryModel.cs
@@ -514,6 +514,10 @@ namespace Toggl.Phoebe.Data.Models
             if (!IsShared || !IsPersisted)
                 throw new InvalidOperationException ("Model needs to be the shared and persisted.");
 
+            // Time entry is already running, nothing to continue
+            if (IsRunning)
+                return;
+
             if (DurationOnly && StartTime.ToLocalTime ().Date == DateTime.Now.Date) {
                 IsRunning = true;
                 return this;

commit 9bbb922f4f7239f82aa5820508848c00269e567a

Fix compilation error introduced in 5da30ae.

Phoebe/Data/Models/TimeEntryModel.cs
@@ -516,7 +516,7 @@ namespace Toggl.Phoebe.Data.Models
 
             // Time entry is already running, nothing to continue
             if (IsRunning)
-                return;
+                return this;
 
             if (DurationOnly && StartTime.ToLocalTime ().Date == DateTime.Now.Date) {
                 IsRunning = true;
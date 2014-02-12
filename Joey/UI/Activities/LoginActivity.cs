6354445528b7835d8b4693c475f3f6e5d47ab3f1
Aleksandr <aleksandr@toggl.com>
2014-02-12 14:08:43 +0200

    Show progress indicator when loggin in #33

--- Joey/UI/Activities/LoginActivity.cs
+++ Joey/UI/Activities/LoginActivity.cs
@@ -8,6 +8,7 @@ using Android.Content;
 using Android.Gms.Auth;
 using Android.Gms.Common;
 using Android.OS;
+using Android.Views;
 using Android.Widget;
 using Toggl.Phoebe;
 using Toggl.Phoebe.Net;
@@ -28,12 +29,15 @@ namespace Toggl.Joey.UI.Activities
 
         protected Button GoogleLoginButton { get; private set; }
 
+        protected ProgressBar LoginProgressBar { get; private set; }
+
         private void FindViews ()
         {
             EmailEditText = FindViewById<AutoCompleteTextView> (Resource.Id.EmailAutoCompleteTextView);
             PasswordEditText = FindViewById<EditText> (Resource.Id.PasswordEditText);
             LoginButton = FindViewById<Button> (Resource.Id.LoginButton);
             GoogleLoginButton = FindViewById<Button> (Resource.Id.GoogleLoginButton);
+            LoginProgressBar = FindViewById<ProgressBar> (Resource.Id.LoginProgressBar);
         }
 
         protected override bool RequireAuth {
@@ -126,6 +130,11 @@ namespace Toggl.Joey.UI.Activities
                 PasswordEditText.Enabled = !value;
                 LoginButton.Enabled = !value;
                 GoogleLoginButton.Enabled = !value;
+                if (value) {
+                    LoginProgressBar.Visibility = ViewStates.Visible;
+                } else {
+                    LoginProgressBar.Visibility = ViewStates.Invisible;
+                }
             }
         }
 

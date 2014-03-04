13e8d98e20d003ef8c40863477908b99287e7f70
Mart Roosmaa <mart@toggl.com>
2014-03-04 11:53:30 +0200

    Convert recent adapter view holders to new base class.

--- Joey/UI/Adapters/RecentTimeEntriesAdapter.cs
+++ Joey/UI/Adapters/RecentTimeEntriesAdapter.cs
@@ -13,6 +13,7 @@ using Toggl.Phoebe.Data;
 using Toggl.Phoebe.Data.Models;
 using Toggl.Phoebe.Data.Views;
 using XPlatUtils;
+using Toggl.Joey.UI.Utils;
 
 namespace Toggl.Joey.UI.Adapters
 {
@@ -35,13 +36,8 @@ namespace Toggl.Joey.UI.Adapters
             return view;
         }
 
-        private class RecentTimeEntryListItemHolder : Java.Lang.Object
+        private class RecentTimeEntryListItemHolder : ModelViewHolder<TimeEntryModel>
         {
-            #pragma warning disable 0414
-            private readonly object subscriptionModelChanged;
-            #pragma warning restore 0414
-            private TimeEntryModel model;
-
             public View ColorView { get; private set; }
 
             public TextView ProjectTextView { get; private set; }
@@ -52,17 +48,11 @@ namespace Toggl.Joey.UI.Adapters
 
             public TextView DescriptionTextView { get; private set; }
 
-            public RecentTimeEntryListItemHolder (View root)
-            {
-                FindViews (root);
-
-                // Cannot use model.OnPropertyChanged callback directly as it would most probably leak memory,
-                // thus the global ModelChangedMessage is used instead.
-                var bus = ServiceContainer.Resolve<MessageBus> ();
-                subscriptionModelChanged = bus.Subscribe<ModelChangedMessage> (OnModelChanged);
+            private TimeEntryModel Model {
+                get { return DataSource; }
             }
 
-            private void FindViews (View root)
+            public RecentTimeEntryListItemHolder (View root) : base (root)
             {
                 ColorView = root.FindViewById<View> (Resource.Id.ColorView);
                 ProjectTextView = root.FindViewById<TextView> (Resource.Id.ProjectTextView);
@@ -71,76 +61,73 @@ namespace Toggl.Joey.UI.Adapters
                 TaskTextView = root.FindViewById<TextView> (Resource.Id.TaskTextView);
             }
 
-            private void OnModelChanged (ModelChangedMessage msg)
+            protected override void OnModelChanged (ModelChangedMessage msg)
             {
                 // Protect against Java side being GCed
                 if (Handle == IntPtr.Zero)
                     return;
 
-                if (model == null)
+                if (Model == null)
                     return;
 
-                if (model == msg.Model) {
+                if (Model == msg.Model) {
                     if (msg.PropertyName == TimeEntryModel.PropertyStartTime
                         || msg.PropertyName == TimeEntryModel.PropertyDescription
                         || msg.PropertyName == TimeEntryModel.PropertyProjectId
                         || msg.PropertyName == TimeEntryModel.PropertyTaskId)
                         Rebind ();
-                } else if (model.ProjectId.HasValue && model.ProjectId == msg.Model.Id) {
+                } else if (Model.ProjectId.HasValue && Model.ProjectId == msg.Model.Id) {
                     if (msg.PropertyName == ProjectModel.PropertyName
                         || msg.PropertyName == ProjectModel.PropertyColor)
                         Rebind ();
-                } else if (model.ProjectId.HasValue && model.Project != null
-                           && model.Project.ClientId.HasValue
-                           && model.Project.ClientId == msg.Model.Id) {
+                } else if (Model.ProjectId.HasValue && Model.Project != null
+                           && Model.Project.ClientId.HasValue
+                           && Model.Project.ClientId == msg.Model.Id) {
                     if (msg.PropertyName == ClientModel.PropertyName)
                         Rebind ();
-                } else if (model.TaskId.HasValue && model.TaskId == msg.Model.Id) {
+                } else if (Model.TaskId.HasValue && Model.TaskId == msg.Model.Id) {
                     if (msg.PropertyName == TaskModel.PropertyName)
                         Rebind ();
                 }
             }
 
-            public void Bind (TimeEntryModel model)
+            protected override void Rebind ()
             {
-                this.model = model;
-                Rebind ();
-            }
+                if (Model == null)
+                    return;
 
-            private void Rebind ()
-            {
                 var ctx = ServiceContainer.Resolve<Context> ();
 
-                if (model.Project != null && model.Project.Client != null) {
-                    ClientTextView.Text = model.Project.Client.Name;
+                if (Model.Project != null && Model.Project.Client != null) {
+                    ClientTextView.Text = Model.Project.Client.Name;
                 } else {
                     ClientTextView.Text = "";
                 }
 
-                if (model.Task != null) {
+                if (Model.Task != null) {
                     //Can't use margin, because with empty task description will still be margined
-                    TaskTextView.Text = model.Task.Name + "  ";
+                    TaskTextView.Text = Model.Task.Name + "  ";
                 } else {
                     TaskTextView.Text = "";
                 }
 
                 var color = Color.Transparent;
-                if (model.Project != null) {
-                    color = Color.ParseColor (model.Project.GetHexColor ());
+                if (Model.Project != null) {
+                    color = Color.ParseColor (Model.Project.GetHexColor ());
                     ProjectTextView.SetTextColor (color);
-                    ProjectTextView.Text = model.Project.Name;
+                    ProjectTextView.Text = Model.Project.Name;
                 } else {
                     ProjectTextView.Text = ctx.GetString (Resource.String.RecentTimeEntryNoProject);
-                    ProjectTextView.SetTextColor (ctx.Resources.GetColor(Resource.Color.dark_gray_text));
+                    ProjectTextView.SetTextColor (ctx.Resources.GetColor (Resource.Color.dark_gray_text));
                 }
 
                 var shape = ColorView.Background as GradientDrawable;
                 shape.SetColor (color);
 
-                if (String.IsNullOrWhiteSpace (model.Description)) {
+                if (String.IsNullOrWhiteSpace (Model.Description)) {
                     DescriptionTextView.Text = ctx.GetString (Resource.String.RecentTimeEntryNoDescription);
                 } else {
-                    DescriptionTextView.Text = model.Description;
+                    DescriptionTextView.Text = Model.Description;
                 }
             }
         }

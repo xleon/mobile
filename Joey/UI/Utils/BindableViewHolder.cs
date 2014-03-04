69f28dc56ef53bc99c813ea919c069e3621e60e9
Mart Roosmaa <mart@toggl.com>
2014-03-04 11:49:54 +0200

    Add base classes for creating view holders.
    The view holder pattern in list views was starting to repeat alot of
    code, thus moving it into a base class.

---
+++ Joey/UI/Utils/BindableViewHolder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Android.Views;
+
+namespace Toggl.Joey.UI.Utils
+{
+    /// <summary>
+    /// Base class for bindable view holders. Useful for list view items.
+    /// </summary>
+    public abstract class BindableViewHolder<T> : Java.Lang.Object
+    {
+        private readonly View root;
+
+        public T DataSource { get; private set; }
+
+        public BindableViewHolder (View root)
+        {
+            this.root = root;
+            root.ViewAttachedToWindow += OnRootAttachedToWindow;
+            root.ViewDetachedFromWindow += OnRootDetachedFromWindow;
+        }
+
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing) {
+                root.ViewAttachedToWindow -= OnRootAttachedToWindow;
+                root.ViewDetachedFromWindow -= OnRootDetachedFromWindow;
+
+                DataSource = default(T);
+            }
+
+            base.Dispose (disposing);
+        }
+
+        protected virtual void OnRootAttachedToWindow (object sender, View.ViewAttachedToWindowEventArgs e)
+        {
+            Rebind ();
+        }
+
+        protected virtual void OnRootDetachedFromWindow (object sender, View.ViewDetachedFromWindowEventArgs e)
+        {
+        }
+
+        public void Bind (T dataSource)
+        {
+            DataSource = dataSource;
+            Rebind ();
+        }
+
+        protected abstract void Rebind ();
+    }
+}

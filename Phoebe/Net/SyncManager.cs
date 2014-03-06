3c53bd8dc17ca9aaa726375581fd92f48aa51e09
Mart Roosmaa <mart@toggl.com>
2014-03-06 13:22:18 +0200

    Extract ModelGraph from SyncManager.
    Moving the ModelGraph class into the light so that it could be tested.

--- Phoebe/Net/SyncManager.cs
+++ Phoebe/Net/SyncManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Toggl.Phoebe.Data;
@@ -120,7 +119,7 @@ namespace Toggl.Phoebe.Net
 
                 if (mode.HasFlag (SyncMode.Push)) {
                     // Construct dependency graph:
-                    var graph = Graph.FromDirty (Enumerable.Empty<Model> ()
+                    var graph = ModelGraph.FromDirty (Enumerable.Empty<Model> ()
                         .Union (QueryDirtyModels<WorkspaceModel> ())
                         .Union (QueryDirtyModels<WorkspaceUserModel> ())
                         .Union (QueryDirtyModels<TagModel> ())
@@ -250,138 +249,6 @@ namespace Toggl.Phoebe.Net
         private DateTime? LastRun {
             get { return ServiceContainer.Resolve<ISettingsStore> ().SyncLastRun; }
             set { ServiceContainer.Resolve<ISettingsStore> ().SyncLastRun = value; }
-        }
-
-        private class Graph
-        {
-            private class Node
-            {
-                public readonly HashSet<Model> Parents = new HashSet<Model> ();
-                public readonly HashSet<Model> Children = new HashSet<Model> ();
-            }
-
-            private readonly Dictionary<Model, Node> nodes = new Dictionary<Model, Node> ();
-
-            public void Add (Model model, Model parent = null)
-            {
-                if (parent != null)
-                    Add (parent);
-
-                Node node;
-                if (!nodes.TryGetValue (model, out node)) {
-                    nodes [model] = node = new Node ();
-                }
-
-                if (parent != null) {
-                    node.Parents.Add (parent);
-                    nodes [parent].Children.Add (model);
-                }
-            }
-
-            public IEnumerable<Model> Remove (Model model)
-            {
-                Node node;
-                if (!nodes.TryGetValue (model, out node))
-                    return Enumerable.Empty<Model> ();
-
-                var removedModels = new List<Model> ();
-                Remove (model, removedModels);
-                return removedModels;
-            }
-
-            private void Remove (Model model, List<Model> deleted)
-            {
-                Node node;
-                if (!nodes.TryGetValue (model, out node))
-                    return;
-
-                // Remove children
-                foreach (var child in node.Children) {
-                    Remove (child, deleted);
-                }
-
-                // Detach from parents
-                foreach (var parent in node.Parents) {
-                    nodes [parent].Children.Remove (model);
-                }
-
-                nodes.Remove (model);
-                deleted.Add (model);
-            }
-
-            public int NodeCount {
-                get { return nodes.Count; }
-            }
-
-            public IEnumerable<Model> Nodes {
-                get { return nodes.Keys; }
-            }
-
-            public IEnumerable<Model> EndNodes {
-                get {
-                    return nodes.Where ((kvp) => kvp.Value.Children.Count == 0)
-                            .Select ((kvp) => kvp.Key);
-                }
-            }
-
-            public IEnumerable<Model> RemoveBranch (Model model)
-            {
-                Node node;
-                if (!nodes.TryGetValue (model, out node))
-                    return Enumerable.Empty<Model> ();
-
-                var removedModels = new List<Model> ();
-
-                // Find elders (highest parents):
-                var elders = new HashSet<Model> ();
-                var parentStack = new Stack<Model> ();
-                parentStack.Push (model);
-                while (parentStack.Count > 0) {
-                    var parent = parentStack.Pop ();
-
-                    var parentNode = nodes [parent];
-                    if (parentNode.Parents.Count == 0) {
-                        elders.Add (parent);
-                    } else {
-                        foreach (var grandparent in parentNode.Parents) {
-                            parentStack.Push (grandparent);
-                        }
-                    }
-                }
-
-                // Remove elders from graph:
-                foreach (var elder in elders) {
-                    Remove (elder, removedModels);
-                }
-
-                return removedModels;
-            }
-
-            public static Graph FromDirty (IEnumerable<Model> models)
-            {
-                var graph = new Graph ();
-
-                Stack<Model> parentStack = new Stack<Model> ();
-                Stack<IEnumerable<Model>> modelsStack = new Stack<IEnumerable<Model>> ();
-
-                parentStack.Push (null);
-                modelsStack.Push (models);
-
-                Model parent;
-                while (parentStack.Count > 0) {
-                    parent = parentStack.Pop ();
-                    models = modelsStack.Pop ()
-                        .Where ((m) => m != null && (m.IsDirty || m.RemoteId == null || m.DeletedAt != null));
-
-                    foreach (var model in models) {
-                        parentStack.Push (model);
-                        modelsStack.Push (model.GetAllForeignModels ().Values);
-                        graph.Add (model, parent);
-                    }
-                }
-
-                return graph;
-            }
         }
     }
 }

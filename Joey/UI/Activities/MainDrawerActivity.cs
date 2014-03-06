1118d741311ab12ca095780e5ac29f4019180e4b
Aleksandr <aleksandr@toggl.com>
2014-03-06 12:08:07 +0200

    Add Drawer icon next to App icon and make it pressable and behave as standard drawer icon should

--- Joey/UI/Activities/MainDrawerActivity.cs
+++ Joey/UI/Activities/MainDrawerActivity.cs
@@ -14,6 +14,7 @@ using XPlatUtils;
 using Toggl.Joey.UI.Adapters;
 using Toggl.Joey.UI.Fragments;
 using Fragment = Android.Support.V4.App.Fragment;
+using Android.Support.V4.App;
 
 namespace Toggl.Joey.UI.Activities
 {
@@ -24,9 +25,9 @@ namespace Toggl.Joey.UI.Activities
     public class MainDrawerActivity : BaseActivity
     {
         private static readonly string LogTag = "MainDrawerActivity";
-
         protected Logger log;
-        protected DrawerLayout DrawerLayout;
+        private DrawerLayout DrawerLayout;
+        protected ActionBarDrawerToggle DrawerToggle;
 
         protected override void OnCreate (Bundle bundle)
         {
@@ -34,12 +35,41 @@ namespace Toggl.Joey.UI.Activities
 
             SetContentView (Resource.Layout.MainDrawerActivity);
 
-            DrawerLayout = FindViewById<DrawerLayout> (Resource.Id.DrawerLayout);
             var drawerList = FindViewById<ListView> (Resource.Id.DrawerListView);
             drawerList.Adapter = new DrawerListAdapter ();
             drawerList.ItemClick += OnDrawerItemClick;
 
+            DrawerLayout = FindViewById<DrawerLayout> (Resource.Id.DrawerLayout);
+            DrawerToggle = new ActionBarDrawerToggle (this, DrawerLayout, Resource.Drawable.IcDrawer, Resource.String.EntryName, Resource.String.EntryName);
+
+            DrawerLayout.SetDrawerShadow (Resource.Drawable.drawershadow, (int)GravityFlags.Start);
+            DrawerLayout.SetDrawerListener (DrawerToggle);
+
+            ActionBar.SetDisplayHomeAsUpEnabled (true);
+            ActionBar.SetHomeButtonEnabled (true);
+
             log = ServiceContainer.Resolve<Logger> ();
+        }
+
+        protected override void OnPostCreate (Bundle savedInstanceState)
+        {
+            base.OnPostCreate (savedInstanceState);
+            DrawerToggle.SyncState ();
+        }
+
+        public override void OnConfigurationChanged (Android.Content.Res.Configuration newConfig)
+        {
+            base.OnConfigurationChanged (newConfig);
+            DrawerToggle.OnConfigurationChanged (newConfig);
+        }
+
+        public override bool OnOptionsItemSelected (IMenuItem item)
+        {
+            if (DrawerToggle.OnOptionsItemSelected (item)) {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected (item);
         }
 
         protected override void OnResume ()
@@ -48,23 +78,23 @@ namespace Toggl.Joey.UI.Activities
             OpenTimeTracking ();
         }
 
-        private void OpenTimeTracking()
+        private void OpenTimeTracking ()
         {
             OpenFragment (new TimeTrackingFragment ());
         }
 
-        private void OpenSettings()
+        private void OpenSettings ()
         {
             OpenFragment (new SettingsFragment ());
         }
 
-        private void OpenFragment(Fragment fragment)
+        private void OpenFragment (Fragment fragment)
         {
             var fragmentTransaction = FragmentManager.BeginTransaction ();
-            fragmentTransaction.Replace (Resource.Id.ContentFrameLayout, fragment).Commit();
+            fragmentTransaction.Replace (Resource.Id.ContentFrameLayout, fragment).Commit ();
         }
 
-        private void OnDrawerItemClick(object sender, ListView.ItemClickEventArgs e)
+        private void OnDrawerItemClick (object sender, ListView.ItemClickEventArgs e)
         {
             log.Debug (LogTag, "Drawer item clicked " + e.Id);
 

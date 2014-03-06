8d3075a00b2039340c910c8662134380572b1506
Mart Roosmaa <mart@toggl.com>
2014-03-06 16:55:21 +0200

    Add stronly typed font representation.

--- Joey/UI/Views/TextViewExtensions.cs
+++ Joey/UI/Views/TextViewExtensions.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using Android.Widget;
+using Android.Graphics;
+using Android.Content;
+using Toggl.Joey.UI.Utils;
 
 namespace Toggl.Joey.UI.Views
 {
-    public class TextViewExtensions
+    public static class TextViewExtensions
     {
-        public TextViewExtensions ()
+        public static T SetFont<T> (this T view, Font font)
+            where T : TextView
         {
+            view.SetTypeface (font.Typeface, font.Style);
+            return view;
         }
     }
 }
-

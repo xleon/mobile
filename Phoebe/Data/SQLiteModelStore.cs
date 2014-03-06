xleon/mobile

commit b3a5095cb2a547a6f75c30f7820852bd660c8b5f

Add log out handling.

Phoebe/Data/SQLiteModelStore.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using SQLite;
+using Toggl.Phoebe.Net;
 using XPlatUtils;
 
 namespace Toggl.Phoebe.Data
@@ -118,6 +119,7 @@ namespace Toggl.Phoebe.Data
         private readonly List<WeakReference> createdModels = new List<WeakReference> ();
         #pragma warning disable 0414
         private readonly Subscription<ModelChangedMessage> modelChangedSubscription;
+        private readonly Subscription<AuthChangedMessage> subscriptionAuthChanged;
         #pragma warning restore 0414
 
         public SQLiteModelStore (string dbPath)
@@ -127,12 +129,20 @@ namespace Toggl.Phoebe.Data
 
             var bus = ServiceContainer.Resolve<MessageBus> ();
             modelChangedSubscription = bus.Subscribe<ModelChangedMessage> (OnModelChangedMessage, threadSafe: true);
+            subscriptionAuthChanged = bus.Subscribe<AuthChangedMessage> (OnAuthChanged);
         }
 
         protected virtual void CreateTables (SQLiteConnection db)
         {
             foreach (var t in Model.GetAllModels()) {
                 db.CreateTable (t);
+            }
+        }
+
+        protected virtual void ClearTables (SQLiteConnection db)
+        {
+            foreach (var t in Model.GetAllModels()) {
+                db.DeleteAll (t);
             }
         }
 
@@ -218,6 +228,17 @@ namespace Toggl.Phoebe.Data
 
                 changedModels.Add (model);
                 ScheduleCommit ();
+            }
+        }
+
+        private void OnAuthChanged (AuthChangedMessage msg)
+        {
+            if (msg.AuthManager.IsAuthenticated)
+                return;
+
+            lock (Model.SyncRoot) {
+                // Wipe database on logout
+                ClearTables ();
             }
         }
 

Phoebe/Data/ModelManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using Toggl.Phoebe.Net;
 using XPlatUtils;
 
 namespace Toggl.Phoebe.Data
@@ -9,6 +10,15 @@ namespace Toggl.Phoebe.Data
     public class ModelManager
     {
         private readonly Dictionary<Type, MemoryModelCache> caches = new Dictionary<Type, MemoryModelCache> ();
+        #pragma warning disable 0414
+        private readonly Subscription<AuthChangedMessage> subscriptionAuthChanged;
+        #pragma warning restore 0414
+
+        public ModelManager ()
+        {
+            var bus = ServiceContainer.Resolve<MessageBus> ();
+            subscriptionAuthChanged = bus.Subscribe<AuthChangedMessage> (OnAuthChanged);
+        }
 
         /// <summary>
         /// Returns all of the cached shared models.
@@ -231,6 +241,17 @@ namespace Toggl.Phoebe.Data
                 if (cache != null) {
                     cache.UpdateRemoteId (model, oldValue, newValue);
                 }
+            }
+        }
+
+        private void OnAuthChanged (AuthChangedMessage msg)
+        {
+            if (msg.AuthManager.IsAuthenticated)
+                return;
+
+            lock (Model.SyncRoot) {
+                // Wipe caches on logout
+                caches.Clear ();
             }
         }
     }

Phoebe/Net/AuthChangedMessage.cs
@@ -7,5 +7,9 @@ namespace Toggl.Phoebe.Net
         public AuthChangedMessage (AuthManager manager) : base (manager)
         {
         }
+
+        public AuthManager AuthManager {
+            get { return (AuthManager)Sender; }
+        }
     }
 }

Phoebe/Net/AuthManager.cs
@@ -104,8 +104,6 @@ namespace Toggl.Phoebe.Net
             Token = null;
             UserId = null;
 
-            // TODO: Clear database
-
             ServiceContainer.Resolve<MessageBus> ().Send (
                 new AuthChangedMessage (this));
         }

Phoebe/Net/SyncManager.cs
@@ -14,17 +14,28 @@ namespace Toggl.Phoebe.Net
         private static readonly string Tag = "SyncManager";
         #pragma warning disable 0414
         private readonly Subscription<ModelsCommittedMessage> subscriptionModelsCommited;
+        private readonly Subscription<AuthChangedMessage> subscriptionAuthChanged;
         #pragma warning restore 0414
 
         public SyncManager ()
         {
             var bus = ServiceContainer.Resolve<MessageBus> ();
             subscriptionModelsCommited = bus.Subscribe<ModelsCommittedMessage> (OnModelsCommited);
+            subscriptionAuthChanged = bus.Subscribe<AuthChangedMessage> (OnAuthChanged);
         }
 
         private void OnModelsCommited (ModelsCommittedMessage msg)
         {
             Run (SyncMode.Auto);
+        }
+
+        private void OnAuthChanged (AuthChangedMessage msg)
+        {
+            if (msg.AuthManager.IsAuthenticated)
+                return;
+
+            // Reset last run on logout
+            LastRun = null;
         }
 
         public async void Run (SyncMode mode = SyncMode.Full)

commit 5f171180abb74e99e69e5eb61ea9536c50c1f082

Fix broken code introduced in b3a5095.

Phoebe/Data/SQLiteModelStore.cs
@@ -142,7 +142,9 @@ namespace Toggl.Phoebe.Data
         protected virtual void ClearTables (SQLiteConnection db)
         {
             foreach (var t in Model.GetAllModels()) {
-                db.DeleteAll (t);
+                var map = db.GetMapping (t);
+                var query = string.Format ("delete from \"{0}\"", map.TableName);
+                db.Execute (query);
             }
         }
 
@@ -238,7 +240,7 @@ namespace Toggl.Phoebe.Data
 
             lock (Model.SyncRoot) {
                 // Wipe database on logout
-                ClearTables ();
+                ClearTables (conn);
             }
         }
 
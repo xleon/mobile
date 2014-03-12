xleon/mobile

commit 4f02620dbe266e0d8606ff5ec0726126db831b99

Prevent loading of log data on app start.
When the app opened, the viewpager started all three activities, meaning
the log activity started loading data from internet. This behaviour was
undesired, so worked around it by loading data when the user starts
scrolling to the log fragment.

Joey/UI/Fragments/RecentTimeEntriesListFragment.cs
@@ -57,7 +57,12 @@ namespace Toggl.Joey.UI.Fragments
             if (welcomeView != null) {
                 ListView.AddHeaderView (welcomeView);
             }
-            ListAdapter = new RecentTimeEntriesAdapter ();
+        }
+
+        public override void OnResume ()
+        {
+            EnsureAdapter ();
+            base.OnResume ();
         }
 
         public override void OnListItemClick (ListView l, View v, int position, long id)
@@ -87,6 +92,21 @@ namespace Toggl.Joey.UI.Fragments
             // Notify that the user explicitly started something
             var bus = ServiceContainer.Resolve<MessageBus> ();
             bus.Send (new UserTimeEntryStateChangeMessage (this, entry));
+        }
+
+        public override bool UserVisibleHint {
+            get { return base.UserVisibleHint; }
+            set {
+                base.UserVisibleHint = value;
+                EnsureAdapter ();
+            }
+        }
+
+        private void EnsureAdapter ()
+        {
+            if (ListAdapter == null && UserVisibleHint) {
+                ListAdapter = new RecentTimeEntriesAdapter ();
+            }
         }
     }
 }

Joey/UI/Fragments/LogTimeEntriesListFragment.cs
@@ -33,7 +33,12 @@ namespace Toggl.Joey.UI.Fragments
             ListView.SetClipToPadding (false);
             ListView.ChoiceMode = (ChoiceMode)AbsListViewChoiceMode.MultipleModal;
             ListView.SetMultiChoiceModeListener (this);
-            ListAdapter = new LogTimeEntriesAdapter ();
+        }
+
+        public override void OnResume ()
+        {
+            EnsureAdapter ();
+            base.OnResume ();
         }
 
         public override void OnListItemClick (ListView l, View v, int position, long id)
@@ -47,6 +52,21 @@ namespace Toggl.Joey.UI.Fragments
                 return;
 
             // TODO Call Edit screen for this row
+        }
+
+        public override bool UserVisibleHint {
+            get { return base.UserVisibleHint; }
+            set {
+                base.UserVisibleHint = value;
+                EnsureAdapter ();
+            }
+        }
+
+        private void EnsureAdapter ()
+        {
+            if (ListAdapter == null && UserVisibleHint) {
+                ListAdapter = new LogTimeEntriesAdapter ();
+            }
         }
 
         void AbsListView.IMultiChoiceModeListener.OnItemCheckedStateChanged (ActionMode mode, int position, long id, bool @checked)

Joey/UI/Fragments/TimeTrackingFragment.cs
@@ -30,6 +30,7 @@ namespace Toggl.Joey.UI.Fragments
         {
             var view = inflater.Inflate (Resource.Layout.TimeTrackingFragment, container, false);
             viewPager = view.FindViewById<ViewPager> (Resource.Id.ViewPager);
+            viewPager.PageScrolled += OnViewPagerPageScrolled;
             viewPager.PageSelected += OnViewPagerPageSelected;
             return view;
         }
@@ -37,6 +38,7 @@ namespace Toggl.Joey.UI.Fragments
         public override void OnDestroyView ()
         {
             viewPager.PageSelected -= OnViewPagerPageSelected;
+            viewPager.PageScrolled -= OnViewPagerPageScrolled;
             base.OnDestroyView ();
         }
 
@@ -89,6 +91,29 @@ namespace Toggl.Joey.UI.Fragments
             }
 
             base.OnPause ();
+        }
+
+        private void OnViewPagerPageScrolled (object sender, ViewPager.PageScrolledEventArgs e)
+        {
+            var current = viewPager.CurrentItem;
+            var pos = e.Position + e.PositionOffset;
+            int idx;
+            if (pos + 0.05f < current) {
+                // Moving to the left
+                idx = (int)Math.Floor (pos);
+            } else if (pos - 0.05f > current) {
+                // Moving to the right
+                idx = (int)Math.Ceiling (pos);
+            } else {
+                return;
+            }
+
+            // Make sure the fragment knows that it's about to become visible
+            var adapter = (MainPagerAdapter)viewPager.Adapter;
+            if (adapter != null) {
+                var frag = adapter.GetItem (idx);
+                frag.UserVisibleHint = true;
+            }
         }
 
         private void OnViewPagerPageSelected (object sender, ViewPager.PageSelectedEventArgs e)

commit d553d29d69c12ac39d477775e60dc39baf06fd80

Fix adding welcome box to recent list.
Welcome box was broken in 4f02620, when lazy loading was introduced.

Joey/UI/Fragments/RecentTimeEntriesListFragment.cs
@@ -54,9 +54,6 @@ namespace Toggl.Joey.UI.Fragments
         {
             base.OnViewCreated (view, savedInstanceState);
             ListView.SetClipToPadding (false);
-            if (welcomeView != null) {
-                ListView.AddHeaderView (welcomeView);
-            }
         }
 
         public override void OnResume ()
@@ -104,7 +101,10 @@ namespace Toggl.Joey.UI.Fragments
 
         private void EnsureAdapter ()
         {
-            if (ListAdapter == null && UserVisibleHint) {
+            if (ListAdapter == null && UserVisibleHint && IsAdded) {
+                if (welcomeView != null) {
+                    ListView.AddHeaderView (welcomeView);
+                }
                 ListAdapter = new RecentTimeEntriesAdapter ();
             }
         }
48743e2381e008b99125940da5ce7789d4f99fbb
Mart Roosmaa <mart@toggl.com>
2014-03-19 12:37:31 +0200

    Prevent showing single dialog multiple times. Fixes #81.

--- Joey/UI/Fragments/BaseDialogFragment.cs
+++ Joey/UI/Fragments/BaseDialogFragment.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Android.Support.V4.App;
 using DialogFragment = Android.Support.V4.App.DialogFragment;
 
 namespace Toggl.Joey.UI.Fragments
@@ -11,6 +12,16 @@ namespace Toggl.Joey.UI.Fragments
 
         protected BaseDialogFragment (IntPtr jref, Android.Runtime.JniHandleOwnership xfer) : base (jref, xfer)
         {
+        }
+
+        public override void Show (FragmentManager manager, string tag)
+        {
+            // Make sure we don't show the dialog twice
+            var frag = manager.FindFragmentByTag (tag);
+            if (frag != null)
+                return;
+
+            base.Show (manager, tag);
         }
     }
 }

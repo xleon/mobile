1dc9d4780896b4c5b7ff47652b3ebde922c503a5
Mart Roosmaa <mart@toggl.com>
2014-04-01 11:46:47 +0300

    Manually commit data on stop broadcast.
    In case the Android system kills our process right after returning from
    the broadcast receiver function we need to be sure that the data is
    actually commited to the database. If it isn't the user gets the
    perception that the time entry was stopped, but in the database it is
    actually still running (and sync did also not happen).

--- Joey/StopTimeEntryBroadcastReceiver.cs
+++ Joey/StopTimeEntryBroadcastReceiver.cs
@@ -1,17 +1,31 @@
 using System;
 using Android.Content;
+using Toggl.Phoebe;
+using Toggl.Phoebe.Data;
 using Toggl.Phoebe.Data.Models;
+using XPlatUtils;
 
 namespace Toggl.Joey
 {
     [BroadcastReceiver (Exported = true)]
     class StopTimeEntryBroadcastReceiver: BroadcastReceiver
     {
+        private const string LogTag = "StopTimeEntryBroadcastReceiver";
+
         public override void OnReceive (Context context, Intent intent)
         {
             TimeEntryModel timeEntry = TimeEntryModel.FindRunning ();
             if (timeEntry != null) {
                 timeEntry.Stop ();
+            }
+
+            // Force commit of data (in case Android kills us right after this function returns)
+            var modelStore = ServiceContainer.Resolve<IModelStore> ();
+            try {
+                modelStore.Commit ();
+            } catch (Exception ex) {
+                var log = ServiceContainer.Resolve<Logger> ();
+                log.Warning (LogTag, ex, "Manual commit failed.");
             }
 
             // Try initialising components

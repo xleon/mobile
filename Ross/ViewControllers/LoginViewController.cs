5596d7cf05f89b0cfcb4694acc762611a6e01f6a
Mart Roosmaa <mart@toggl.com>
2014-04-08 16:18:37 +0300

    Add Pixate component.

--- Ross/ViewControllers/LoginViewController.cs
+++ Ross/ViewControllers/LoginViewController.cs
@@ -5,17 +5,12 @@ namespace Toggl.Ross.ViewControllers
 {
     public class LoginViewController : UIViewController
     {
-        public LoginViewController ()
-        {
-        }
-
         public override void LoadView ()
         {
-            View = new UILabel () {
-                BackgroundColor = UIColor.White,
-                TextAlignment = UITextAlignment.Center,
+            var label = new UILabel () {
                 Text = "LoginHeaderText".Tr (),
             };
+            View = label;
         }
     }
 }

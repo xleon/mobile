c8c5fd0f39e5c89945af59e17cd1d4d4456452e7
Mart Roosmaa <mart@toggl.com>
2014-04-17 13:02:55 +0300

    Fix typo.

--- Phoebe/Net/SyncManager.cs
+++ Phoebe/Net/SyncManager.cs
@@ -200,7 +200,7 @@ namespace Toggl.Phoebe.Net
                     modelStore.TryCommit ();
                 }
             } catch (Exception e) {
-                if (e.IsNetworkFailure () || ex is TaskCanceledException) {
+                if (e.IsNetworkFailure () || e is TaskCanceledException) {
                     log.Info (Tag, e, "Sync ({0}) failed.", mode);
                 } else {
                     log.Warning (Tag, e, "Sync ({0}) failed.", mode);

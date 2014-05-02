0e26a6669498d50d512606bb9c4ecb87611a5a91
Mart Roosmaa <mart@toggl.com>
2014-05-02 15:02:31 +0300

    Add recent view. Closes #143.

---
+++ Ross/Theme/Style.Recent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using MonoTouch.UIKit;
+
+namespace Toggl.Ross.Theme
+{
+    public static partial class Style
+    {
+        public static class Recent
+        {
+            public static void HeaderBackgroundView (UIView v)
+            {
+                v.BackgroundColor = Color.LightGray;
+            }
+
+            public static void HeaderLabel (UILabel v)
+            {
+                v.TextColor = Color.Gray;
+                v.TextAlignment = UITextAlignment.Left;
+                v.Font = UIFont.FromName ("HelveticaNeue-Medium", 14f);
+            }
+
+            public static void CellContentView (UIView v)
+            {
+                v.ApplyStyle (TimeEntryCell.ContentView);
+            }
+
+            public static void CellProjectLabel (UILabel v)
+            {
+                v.ApplyStyle (TimeEntryCell.ProjectLabel);
+            }
+
+            public static void CellClientLabel (UILabel v)
+            {
+                v.ApplyStyle (TimeEntryCell.ClientLabel);
+            }
+
+            public static void CellTaskLabel (UILabel v)
+            {
+                v.ApplyStyle (TimeEntryCell.TaskLabel);
+            }
+
+            public static void CellDescriptionLabel (UILabel v)
+            {
+                v.ApplyStyle (TimeEntryCell.DescriptionLabel);
+            }
+
+            public static void CellDurationLabel (UILabel v)
+            {
+                v.ApplyStyle (TimeEntryCell.DurationLabel);
+            }
+
+            public static void CellTaskDescriptionSeparator (UIImageView v)
+            {
+                v.ApplyStyle (TimeEntryCell.TaskDescriptionSeparator);
+            }
+
+            public static void CellRunningIndicator (UIImageView v)
+            {
+                v.ApplyStyle (TimeEntryCell.RunningIndicator);
+            }
+        }
+    }
+}

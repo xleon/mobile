13d565b42ffc7f2224b34a101474b3934ccd4881
Mart Roosmaa <mart@toggl.com>
2014-05-02 11:46:48 +0300

    Add navigation menu content.

--- Ross/ViewControllers/NavigationMenuController.cs
+++ Ross/ViewControllers/NavigationMenuController.cs
@@ -3,6 +3,8 @@ using System.Drawing;
 using MonoTouch.CoreGraphics;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using Toggl.Phoebe.Net;
+using XPlatUtils;
 using Toggl.Ross.Theme;
 
 namespace Toggl.Ross.ViewControllers
@@ -10,10 +12,16 @@ namespace Toggl.Ross.ViewControllers
     public class NavigationMenuController
     {
         private UIViewController controller;
-
-        public NavigationMenuController ()
-        {
-        }
+        private UIView containerView;
+        private UIView menuView;
+        private UIDynamicAnimator menuAnimator;
+        private UIButton logButton;
+        private UIButton recentButton;
+        private UIButton settingsButton;
+        private UIButton signOutButton;
+        private UIButton[] menuButtons;
+        private UIView[] separators;
+        private bool menuShown;
 
         public void Attach (UIViewController controller)
         {
@@ -23,11 +31,6 @@ namespace Toggl.Ross.ViewControllers
                 Image.IconNav.ImageWithRenderingMode (UIImageRenderingMode.AlwaysOriginal),
                 UIBarButtonItemStyle.Plain, OnNavigationButtonTouched);
         }
-
-        private UIView containerView;
-        private UIView menuView;
-        private UIDynamicAnimator menuAnimator;
-        private bool menuShown;
 
         private void EnsureViews ()
         {
@@ -42,20 +45,78 @@ namespace Toggl.Ross.ViewControllers
                 ClipsToBounds = true,
             };
 
-            menuView = new UIView () {
-                BackgroundColor = UIColor.Black.ColorWithAlpha (0.90f),
-            };
+            menuView = new UIView ().ApplyStyle (Style.NavMenu.Background);
 
+            menuButtons = new[] {
+                (recentButton = new UIButton ()),
+                (logButton = new UIButton ()),
+                (settingsButton = new UIButton ()),
+                (signOutButton = new UIButton ()),
+            };
+            recentButton.SetTitle ("NavMenuRecent".Tr (), UIControlState.Normal);
+            logButton.SetTitle ("NavMenuLog".Tr (), UIControlState.Normal);
+            settingsButton.SetTitle ("NavMenuSettings".Tr (), UIControlState.Normal);
+            signOutButton.SetTitle ("NavMenuSignOut".Tr (), UIControlState.Normal);
+
+            foreach (var menuButton in menuButtons) {
+                var isActive = (menuButton == recentButton && controller is RecentViewController)
+                               || (menuButton == logButton && controller is LogViewController);
+
+                if (isActive) {
+                    menuButton.ApplyStyle (Style.NavMenu.HighlightedItem);
+                } else {
+                    menuButton.ApplyStyle (Style.NavMenu.NormalItem);
+                }
+                menuButton.TouchUpInside += OnMenuButtonTouchUpInside;
+            }
+
+            separators = new UIView[menuButtons.Length - 1];
+            for (var i = 0; i < separators.Length; i++) {
+                separators [i] = new UIView ().ApplyStyle (Style.NavMenu.Separator);
+            }
+
+            menuView.AddSubviews (separators);
+            menuView.AddSubviews (menuButtons);
             containerView.AddSubview (menuView);
 
             menuAnimator = new UIDynamicAnimator (containerView);
             menuAnimator.Delegate = new AnimatorDelegate (this);
 
+            // Layout items:
+            var offsetY = 15f;
+            var sepIdx = 0;
+            foreach (var menuButton in menuButtons) {
+                menuButton.SizeToFit ();
+
+                var frame = menuButton.Frame;
+                frame.Width = navController.View.Frame.Width;
+                frame.Y = offsetY;
+                menuButton.Frame = frame;
+
+                offsetY += frame.Height;
+
+                // Position separator
+                if (sepIdx < separators.Length) {
+                    var separator = separators [sepIdx];
+                    var rightMargin = menuButton.ContentEdgeInsets.Right;
+                    separator.Frame = new RectangleF (
+                        x: rightMargin,
+                        y: offsetY,
+                        width: navController.View.Frame.Width - rightMargin,
+                        height: 1f
+                    );
+
+                    sepIdx += 1;
+                    offsetY += separator.Frame.Height;
+                }
+            }
+            offsetY += 15f;
+
             containerView.Frame = new RectangleF (
                 x: 0,
                 y: navController.NavigationBar.Frame.Bottom,
                 width: navController.View.Frame.Width,
-                height: 250f
+                height: offsetY
             );
 
             menuView.Frame = new RectangleF (
@@ -68,7 +129,30 @@ namespace Toggl.Ross.ViewControllers
             return;
         }
 
+        private void OnMenuButtonTouchUpInside (object sender, EventArgs e)
+        {
+            if (sender == recentButton && !(controller is RecentViewController)) {
+                var navController = controller.NavigationController;
+                navController.SetViewControllers (new[] { new RecentViewController () }, true);
+                // TODO: Store user selection?
+            } else if (sender == logButton && !(controller is LogViewController)) {
+                var navController = controller.NavigationController;
+                navController.SetViewControllers (new[] { new LogViewController () }, true);
+                // TODO: Store user selection?
+            } else if (sender == settingsButton) {
+            } else if (sender == signOutButton) {
+                ServiceContainer.Resolve<AuthManager> ().Forget ();
+            }
+
+            ToggleMenu ();
+        }
+
         private void OnNavigationButtonTouched (object sender, EventArgs e)
+        {
+            ToggleMenu ();
+        }
+
+        private void ToggleMenu ()
         {
             EnsureViews ();
 
---
+++ Ross/ViewControllers/RecentViewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MonoTouch.UIKit;
+
+namespace Toggl.Ross.ViewControllers
+{
+    public class RecentViewController : BaseTimerTableViewController
+    {
+        private readonly NavigationMenuController navMenuController;
+
+        public RecentViewController () : base (UITableViewStyle.Plain)
+        {
+            navMenuController = new NavigationMenuController ();
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+
+            navMenuController.Attach (this);
+        }
+    }
+}

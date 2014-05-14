f14d213e764341313788e8b7f334be1bff54df09
Mart Roosmaa <mart@toggl.com>
2014-05-14 15:43:58 +0300

    Style project cell.

--- Ross/Theme/Style.ProjectList.cs
+++ Ross/Theme/Style.ProjectList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using MonoTouch.UIKit;
 
 namespace Toggl.Ross.Theme
@@ -17,6 +18,62 @@ namespace Toggl.Ross.Theme
                 v.TextColor = Color.Gray;
                 v.TextAlignment = UITextAlignment.Left;
                 v.Font = UIFont.FromName ("HelveticaNeue-Medium", 14f);
+            }
+
+            public static void ProjectLabel (UILabel v)
+            {
+                v.Font = UIFont.FromName ("HelveticaNeue", 17f);
+                v.TextAlignment = UITextAlignment.Left;
+                v.TextColor = Color.White;
+            }
+
+            public static void NoProjectLabel (UILabel v)
+            {
+                v.Font = UIFont.FromName ("HelveticaNeue", 17f);
+                v.TextAlignment = UITextAlignment.Left;
+                v.TextColor = Color.LightGray;
+            }
+
+            public static void NewProjectLabel (UILabel v)
+            {
+                v.Font = UIFont.FromName ("HelveticaNeue", 17f);
+                v.TextAlignment = UITextAlignment.Center;
+                v.TextColor = Color.Gray;
+            }
+
+            public static void ClientLabel (UILabel v)
+            {
+                v.Font = UIFont.FromName ("HelveticaNeue", 13f);
+                v.TextAlignment = UITextAlignment.Left;
+                v.TextColor = Color.White.ColorWithAlpha (0.75f);
+            }
+
+            public static void TasksButtons (UIButton v)
+            {
+                v.Font = UIFont.FromName ("HelveticaNeue-Medium", 17f);
+                v.SetBackgroundImage (TasksBackgroundDefault.Value, UIControlState.Normal);
+                v.SetBackgroundImage (TasksBackgroundHighlighted.Value, UIControlState.Highlighted);
+            }
+
+            private static Lazy<UIImage> TasksBackgroundDefault = new Lazy<UIImage> (() => MakeTasksBackground (Color.White));
+            private static Lazy<UIImage> TasksBackgroundHighlighted = new Lazy<UIImage> (() => MakeTasksBackground (Color.White.ColorWithAlpha (0.75f)));
+
+            private static UIImage MakeTasksBackground (UIColor circleColor)
+            {
+                const int imageSize = 60;
+                const float circleDiameter = 30;
+
+                UIGraphics.BeginImageContextWithOptions (new SizeF (imageSize, imageSize), false, UIScreen.MainScreen.Scale);
+                var ctx = UIGraphics.GetCurrentContext ();
+
+                ctx.SetFillColorWithColor (circleColor.CGColor);
+                ctx.AddArc (imageSize / 2f, imageSize / 2f, circleDiameter / 2f, 0, (float)(2 * Math.PI), true);
+                ctx.FillPath ();
+
+                var image = UIGraphics.GetImageFromCurrentImageContext ();
+                UIGraphics.EndImageContext ();
+
+                return image;
             }
         }
     }
--- Ross/ViewControllers/ProjectSelectionViewController.cs
+++ Ross/ViewControllers/ProjectSelectionViewController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using MonoTouch.CoreAnimation;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using Toggl.Phoebe;
@@ -16,6 +17,8 @@ namespace Toggl.Ross.ViewControllers
 {
     public class ProjectSelectionViewController : UITableViewController
     {
+        private const float CellSpacing = 4f;
+
         public ProjectSelectionViewController (TimeEntryModel model) : base (UITableViewStyle.Plain)
         {
             Title = "ProjectTitle".Tr ();
@@ -181,9 +184,103 @@ namespace Toggl.Ross.ViewControllers
         private class ProjectCell : BindableTableViewCell<ProjectAndTaskView.Project>
         {
             private Subscription<ModelChangedMessage> subscriptionModelChanged;
+            private UIView textContentView;
+            private UILabel projectLabel;
+            private UILabel clientLabel;
+            private UIButton tasksButton;
 
             public ProjectCell (IntPtr handle) : base (handle)
             {
+                BackgroundView = new UIView ();
+
+                ContentView.Add (textContentView = new UIView ());
+                ContentView.Add (tasksButton = new UIButton ().Apply (Style.ProjectList.TasksButtons));
+                textContentView.Add (projectLabel = new UILabel ().Apply (Style.ProjectList.ProjectLabel));
+                textContentView.Add (clientLabel = new UILabel ().Apply (Style.ProjectList.ClientLabel));
+
+                var maskLayer = new CAGradientLayer () {
+                    AnchorPoint = PointF.Empty,
+                    StartPoint = new PointF (0.0f, 0.0f),
+                    EndPoint = new PointF (1.0f, 0.0f),
+                    Colors = new [] {
+                        UIColor.FromWhiteAlpha (1, 1).CGColor,
+                        UIColor.FromWhiteAlpha (1, 1).CGColor,
+                        UIColor.FromWhiteAlpha (1, 0).CGColor,
+                    },
+                    Locations = new [] {
+                        NSNumber.FromFloat (0f),
+                        NSNumber.FromFloat (0.9f),
+                        NSNumber.FromFloat (1f),
+                    },
+                };
+                textContentView.Layer.Mask = maskLayer;
+            }
+
+            public override void LayoutSubviews ()
+            {
+                base.LayoutSubviews ();
+
+                var contentFrame = new RectangleF (0, CellSpacing / 2, Frame.Width, Frame.Height - CellSpacing);
+                SelectedBackgroundView.Frame = BackgroundView.Frame = ContentView.Frame = contentFrame;
+
+                if (!tasksButton.Hidden) {
+                    var virtualWidth = contentFrame.Height;
+                    var buttonWidth = tasksButton.CurrentBackgroundImage.Size.Width;
+                    var extraPadding = (virtualWidth - buttonWidth) / 2f;
+                    tasksButton.Frame = new RectangleF (
+                        contentFrame.Width - virtualWidth + extraPadding, extraPadding,
+                        buttonWidth, buttonWidth);
+                    contentFrame.Width -= virtualWidth;
+                }
+
+                contentFrame.X += 13f;
+                contentFrame.Width -= 13f;
+                textContentView.Frame = contentFrame;
+                textContentView.Layer.Mask.Bounds = contentFrame;
+
+                contentFrame = new RectangleF (PointF.Empty, contentFrame.Size);
+
+                if (clientLabel.Hidden) {
+                    // Only display single item, so make it fill the whole text frame
+                    var bounds = GetBoundingRect (projectLabel);
+                    projectLabel.Frame = new RectangleF (
+                        x: 0,
+                        y: (contentFrame.Height - bounds.Height + projectLabel.Font.Descender) / 2f,
+                        width: contentFrame.Width,
+                        height: bounds.Height
+                    );
+                } else {
+                    // Carefully craft the layout
+                    var bounds = GetBoundingRect (projectLabel);
+                    projectLabel.Frame = new RectangleF (
+                        x: 0,
+                        y: (contentFrame.Height - bounds.Height + projectLabel.Font.Descender) / 2f,
+                        width: bounds.Width,
+                        height: bounds.Height
+                    );
+
+                    const float clientLeftMargin = 7.5f;
+                    bounds = GetBoundingRect (clientLabel);
+                    clientLabel.Frame = new RectangleF (
+                        x: projectLabel.Frame.X + projectLabel.Frame.Width + clientLeftMargin,
+                        y: (float)Math.Floor (projectLabel.Frame.Y + projectLabel.Font.Ascender - clientLabel.Font.Ascender),
+                        width: bounds.Width,
+                        height: bounds.Height
+                    );
+                }
+            }
+
+            private static RectangleF GetBoundingRect (UILabel view)
+            {
+                var attrs = new UIStringAttributes () {
+                    Font = view.Font,
+                };
+                var rect = ((NSString)(view.Text ?? String.Empty)).GetBoundingRect (
+                               new SizeF (Single.MaxValue, Single.MaxValue),
+                               NSStringDrawingOptions.UsesLineFragmentOrigin,
+                               attrs, null);
+                rect.Height = (float)Math.Ceiling (rect.Height);
+                return rect;
             }
 
             protected override void Dispose (bool disposing)
@@ -228,6 +325,56 @@ namespace Toggl.Ross.ViewControllers
 
             protected override void Rebind ()
             {
+                UIColor projectColor;
+                string projectName;
+                string clientName = String.Empty;
+                int taskCount = 0;
+
+                if (DataSource.IsNoProject) {
+                    projectColor = Color.Gray;
+                    projectName = "ProjectNoProject".Tr ();
+                    projectLabel.Apply (Style.ProjectList.NoProjectLabel);
+                } else if (DataSource.IsNewProject) {
+                    projectColor = Color.LightGray;
+                    projectName = "ProjectNewProject".Tr ();
+                    projectLabel.Apply (Style.ProjectList.NewProjectLabel);
+                } else {
+                    var project = DataSource.Model;
+                    projectColor = UIColor.Clear.FromHex (project.GetHexColor ());
+
+                    projectName = project.Name;
+                    clientName = project.Client != null ? project.Client.Name : String.Empty;
+                    taskCount = DataSource.Tasks.Count;
+                    projectLabel.Apply (Style.ProjectList.ProjectLabel);
+                }
+
+                if (String.IsNullOrWhiteSpace (projectName)) {
+                    projectName = "ProjectNoNameProject".Tr ();
+                    clientName = String.Empty;
+                }
+
+                if (!String.IsNullOrWhiteSpace (projectName)) {
+                    projectLabel.Text = projectName;
+                    projectLabel.Hidden = false;
+
+                    if (!String.IsNullOrEmpty (clientName)) {
+                        clientLabel.Text = clientName;
+                        clientLabel.Hidden = false;
+                    } else {
+                        clientLabel.Hidden = true;
+                    }
+                } else {
+                    projectLabel.Hidden = true;
+                    clientLabel.Hidden = true;
+                }
+
+                tasksButton.Hidden = taskCount < 1;
+                if (!tasksButton.Hidden) {
+                    tasksButton.SetTitle (taskCount.ToString (), UIControlState.Normal);
+                    tasksButton.SetTitleColor (projectColor, UIControlState.Normal);
+                }
+
+                BackgroundView.BackgroundColor = projectColor;
             }
         }
 
@@ -235,6 +382,7 @@ namespace Toggl.Ross.ViewControllers
         {
             public TaskCell (IntPtr handle) : base (handle)
             {
+                BackgroundView = new UIView () { BackgroundColor = UIColor.White };
             }
 
             protected override void Rebind ()

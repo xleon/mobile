c95b9cdb54274923f2c1778bfc75c3b7cfbe3571
Mart Roosmaa <mart@toggl.com>
2014-05-15 13:36:25 +0300

    Change data source hiearchy.

--- Ross/DataSources/GroupedDataViewSource.cs
+++ Ross/DataSources/GroupedDataViewSource.cs
@@ -1,24 +1,182 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using MonoTouch.CoreFoundation;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using Toggl.Phoebe.Data.Views;
 
 namespace Toggl.Ross.DataSources
 {
-    public abstract class GroupedDataViewSource<TData, TSection, TRow> : PlainDataViewSource<TData>
+    public abstract class GroupedDataViewSource<TData, TSection, TRow> : UITableViewSource
     {
+        private readonly UITableView tableView;
+        private readonly IDataView<TData> dataView;
+        private UIActivityIndicatorView defaultFooterView;
         private DataCache cache;
 
-        protected GroupedDataViewSource (UITableView tableView, IDataView<TData> dataView) : base (tableView, dataView)
+        protected GroupedDataViewSource (UITableView tableView, IDataView<TData> dataView)
         {
+            this.tableView = tableView;
+            this.dataView = dataView;
+            dataView.Updated += OnDataViewUpdated;
         }
 
-        public override void Attach ()
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing) {
+                dataView.Updated -= OnDataViewUpdated;
+                if (tableView.Source == this) {
+                    tableView.Source = null;
+                }
+            }
+            base.Dispose (disposing);
+        }
+
+        public virtual void Attach ()
         {
             cache = new DataCache (this);
-            base.Attach ();
+            tableView.Source = this;
+            UpdateFooter ();
+        }
+
+
+        public override void Scrolled (UIScrollView scrollView)
+        {
+            TryLoadMore ();
+        }
+
+        private void TryLoadMore ()
+        {
+            var currentOffset = tableView.ContentOffset.Y;
+            var maximumOffset = tableView.ContentSize.Height - tableView.Frame.Height;
+
+            if (maximumOffset - currentOffset <= 200.0) {
+                // Automatically load more
+                if (!dataView.IsLoading && dataView.HasMore) {
+                    dataView.LoadMore ();
+                }
+            }
+        }
+
+        private void OnDataViewUpdated (object sender, EventArgs e)
+        {
+            if (Handle == IntPtr.Zero)
+                return;
+            ScheduleUpdate ();
+            TryLoadMore ();
+        }
+
+        private bool updateScheduled;
+
+        protected void ScheduleUpdate ()
+        {
+            if (updateScheduled)
+                return;
+            updateScheduled = true;
+
+            // For whatever reason we need to dispatch the message again in order for the data to update
+            // during scroll animation.
+            DispatchQueue.MainQueue.DispatchAsync (delegate {
+                updateScheduled = false;
+                if (tableView.Source == this) {
+                    Update ();
+                }
+            });
+        }
+
+
+        protected virtual void Update ()
+        {
+            var oldCache = cache;
+            var newCache = cache = new DataCache (this);
+
+            tableView.BeginUpdates ();
+
+            // Find sections and rows to delete:
+            var sectionIdx = 0;
+            foreach (var section in oldCache.GetSections()) {
+                if (!newCache.GetSections ().Contains (section)) {
+                    tableView.DeleteSections (new NSIndexSet ((uint)sectionIdx), UITableViewRowAnimation.Automatic);
+                } else {
+                    var oldRows = oldCache.GetRows (section);
+                    var newRows = newCache.GetRows (section);
+                    var rowIdx = 0;
+                    foreach (var row in oldRows) {
+                        if (!newRows.Contains (row)) {
+                            tableView.DeleteRows (new[] { NSIndexPath.FromRowSection (rowIdx, sectionIdx) }, UITableViewRowAnimation.Automatic);
+                        }
+
+                        rowIdx += 1;
+                    }
+                }
+
+                sectionIdx += 1;
+            }
+
+            // Determine new items and moved items
+            sectionIdx = 0;
+            foreach (var section in newCache.GetSections()) {
+                var sectionOldIdx = oldCache.GetSections ().IndexOf (section);
+                if (sectionOldIdx < 0) {
+                    // New section needs to be inserted
+                    tableView.InsertSections (new NSIndexSet ((uint)sectionIdx), UITableViewRowAnimation.Automatic);
+                } else {
+                    if (sectionIdx != sectionOldIdx) {
+                        // Old section has changed idx, mark as moved
+                        tableView.MoveSection (sectionOldIdx, sectionIdx);
+                    }
+
+                    var oldRows = oldCache.GetRows (section);
+                    var newRows = newCache.GetRows (section);
+                    var rowIdx = 0;
+                    foreach (var row in newRows) {
+                        var rowOldIdx = oldRows.IndexOf (row);
+                        if (rowOldIdx < 0) {
+                            // New row needs to be inserted
+                            tableView.InsertRows (new[] { NSIndexPath.FromRowSection (rowIdx, sectionIdx) }, UITableViewRowAnimation.Automatic);
+                        } else if (rowIdx != rowOldIdx) {
+                            // Old row has changed idx, mark as moved
+                            tableView.MoveRow (
+                                NSIndexPath.FromRowSection (rowOldIdx, sectionOldIdx),
+                                NSIndexPath.FromRowSection (rowIdx, sectionIdx));
+                        }
+
+                        rowIdx += 1;
+                    }
+                }
+
+                sectionIdx += 1;
+            }
+
+            tableView.EndUpdates ();
+
+            UpdateFooter ();
+        }
+
+        protected virtual void UpdateFooter ()
+        {
+            if (dataView.HasMore || dataView.IsLoading) {
+                if (defaultFooterView == null) {
+                    defaultFooterView = new UIActivityIndicatorView (UIActivityIndicatorViewStyle.Gray);
+                    defaultFooterView.Frame = new RectangleF (0, 0, 50, 50);
+                }
+                tableView.TableFooterView = defaultFooterView;
+                defaultFooterView.StartAnimating ();
+            } else {
+                tableView.TableFooterView = null;
+            }
+        }
+
+        public override int NumberOfSections (UITableView tableView)
+        {
+            return cache.GetSections ().Count;
+        }
+
+        public override int RowsInSection (UITableView tableview, int section)
+        {
+            return GetCachedRows (GetSection (section)).Count;
         }
 
         protected List<TSection> GetCachedSections ()
@@ -41,87 +199,17 @@ namespace Toggl.Ross.DataSources
             return GetCachedRows (GetSection (indexPath.Section)) [indexPath.Row];
         }
 
-        protected override void Update ()
-        {
-            var oldCache = cache;
-            var newCache = cache = new DataCache (this);
+        public UITableView TableView {
+            get { return tableView; }
+        }
 
-            TableView.BeginUpdates ();
-
-            // Find sections and rows to delete:
-            var sectionIdx = 0;
-            foreach (var section in oldCache.GetSections()) {
-                if (!newCache.GetSections ().Contains (section)) {
-                    TableView.DeleteSections (new NSIndexSet ((uint)sectionIdx), UITableViewRowAnimation.Automatic);
-                } else {
-                    var oldRows = oldCache.GetRows (section);
-                    var newRows = newCache.GetRows (section);
-                    var rowIdx = 0;
-                    foreach (var row in oldRows) {
-                        if (!newRows.Contains (row)) {
-                            TableView.DeleteRows (new[] { NSIndexPath.FromRowSection (rowIdx, sectionIdx) }, UITableViewRowAnimation.Automatic);
-                        }
-
-                        rowIdx += 1;
-                    }
-                }
-
-                sectionIdx += 1;
-            }
-
-            // Determine new items and moved items
-            sectionIdx = 0;
-            foreach (var section in newCache.GetSections()) {
-                var sectionOldIdx = oldCache.GetSections ().IndexOf (section);
-                if (sectionOldIdx < 0) {
-                    // New section needs to be inserted
-                    TableView.InsertSections (new NSIndexSet ((uint)sectionIdx), UITableViewRowAnimation.Automatic);
-                } else {
-                    if (sectionIdx != sectionOldIdx) {
-                        // Old section has changed idx, mark as moved
-                        TableView.MoveSection (sectionOldIdx, sectionIdx);
-                    }
-
-                    var oldRows = oldCache.GetRows (section);
-                    var newRows = newCache.GetRows (section);
-                    var rowIdx = 0;
-                    foreach (var row in newRows) {
-                        var rowOldIdx = oldRows.IndexOf (row);
-                        if (rowOldIdx < 0) {
-                            // New row needs to be inserted
-                            TableView.InsertRows (new[] { NSIndexPath.FromRowSection (rowIdx, sectionIdx) }, UITableViewRowAnimation.Automatic);
-                        } else if (rowIdx != rowOldIdx) {
-                            // Old row has changed idx, mark as moved
-                            TableView.MoveRow (
-                                NSIndexPath.FromRowSection (rowOldIdx, sectionOldIdx),
-                                NSIndexPath.FromRowSection (rowIdx, sectionIdx));
-                        }
-
-                        rowIdx += 1;
-                    }
-                }
-
-                sectionIdx += 1;
-            }
-
-            TableView.EndUpdates ();
-
-            UpdateFooter ();
+        public IDataView<TData> DataView {
+            get { return dataView; }
         }
 
         protected abstract IEnumerable<TSection> GetSections ();
 
         protected abstract IEnumerable<TRow> GetRows (TSection section);
-
-        public override int NumberOfSections (UITableView tableView)
-        {
-            return cache.GetSections ().Count;
-        }
-
-        public override int RowsInSection (UITableView tableview, int section)
-        {
-            return GetCachedRows (GetSection (section)).Count;
-        }
 
         private class DataCache
         {
--- Ross/DataSources/PlainDataViewSource.cs
+++ Ross/DataSources/PlainDataViewSource.cs
@@ -1,118 +1,24 @@
 ﻿using System;
-using System.Drawing;
-using MonoTouch.CoreFoundation;
+using System.Collections.Generic;
 using MonoTouch.UIKit;
 using Toggl.Phoebe.Data.Views;
 
 namespace Toggl.Ross.DataSources
 {
-    public abstract class PlainDataViewSource<T> : UITableViewSource
+    public abstract class PlainDataViewSource<T> : GroupedDataViewSource<T, string, T>
     {
-        private readonly UITableView tableView;
-        private readonly IDataView<T> dataView;
-        private UIActivityIndicatorView defaultFooterView;
-
-        protected PlainDataViewSource (UITableView tableView, IDataView<T> dataView)
+        protected PlainDataViewSource (UITableView tableView, IDataView<T> dataView) : base (tableView, dataView)
         {
-            this.tableView = tableView;
-            this.dataView = dataView;
-            dataView.Updated += OnDataViewUpdated;
         }
 
-        protected override void Dispose (bool disposing)
+        protected override IEnumerable<string> GetSections ()
         {
-            if (disposing) {
-                dataView.Updated -= OnDataViewUpdated;
-                if (tableView.Source == this) {
-                    tableView.Source = null;
-                }
-            }
-            base.Dispose (disposing);
+            yield return String.Empty;
         }
 
-        public virtual void Attach ()
+        protected override IEnumerable<T> GetRows (string section)
         {
-            tableView.Source = this;
-            UpdateFooter ();
-        }
-
-        public override void Scrolled (UIScrollView scrollView)
-        {
-            TryLoadMore ();
-        }
-
-        private void TryLoadMore ()
-        {
-            var currentOffset = tableView.ContentOffset.Y;
-            var maximumOffset = tableView.ContentSize.Height - tableView.Frame.Height;
-
-            if (maximumOffset - currentOffset <= 200.0) {
-                // Automatically load more
-                if (!dataView.IsLoading && dataView.HasMore) {
-                    dataView.LoadMore ();
-                }
-            }
-        }
-
-        private void OnDataViewUpdated (object sender, EventArgs e)
-        {
-            if (Handle == IntPtr.Zero)
-                return;
-            ScheduleUpdate ();
-            TryLoadMore ();
-        }
-
-        private bool updateScheduled;
-
-        protected void ScheduleUpdate ()
-        {
-            if (updateScheduled)
-                return;
-            updateScheduled = true;
-
-            // For whatever reason we need to dispatch the message again in order for the data to update
-            // during scroll animation.
-            DispatchQueue.MainQueue.DispatchAsync (delegate {
-                updateScheduled = false;
-                if (tableView.Source == this) {
-                    Update ();
-                }
-            });
-        }
-
-        protected virtual void Update ()
-        {
-            tableView.ReloadData ();
-            UpdateFooter ();
-        }
-
-        protected virtual void UpdateFooter ()
-        {
-            if (dataView.HasMore || dataView.IsLoading) {
-                if (defaultFooterView == null) {
-                    defaultFooterView = new UIActivityIndicatorView (UIActivityIndicatorViewStyle.Gray);
-                    defaultFooterView.Frame = new RectangleF (0, 0, 50, 50);
-                }
-                tableView.TableFooterView = defaultFooterView;
-                defaultFooterView.StartAnimating ();
-            } else {
-                tableView.TableFooterView = null;
-            }
-        }
-
-        public override int NumberOfSections (UITableView tableView)
-        {
-            return 1;
-        }
-
-        public override int RowsInSection (UITableView tableview, int section)
-        {
-            return (int)dataView.Count;
-        }
-
-        public UITableView TableView {
-            get { return tableView; }
+            return DataView.Data;
         }
     }
 }
-

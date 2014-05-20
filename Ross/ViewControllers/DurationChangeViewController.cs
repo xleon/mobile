52904d29f907c9dd432d9f4db1b9803caa72aa9b
Mart Roosmaa <mart@toggl.com>
2014-05-20 11:18:17 +0300

    Add save action to duration view controller.

--- Ross/ViewControllers/DurationChangeViewController.cs
+++ Ross/ViewControllers/DurationChangeViewController.cs
@@ -1,13 +1,73 @@
 ﻿using System;
 using MonoTouch.UIKit;
 using Toggl.Phoebe.Data.Models;
+using Toggl.Ross.Theme;
+using Toggl.Ross.Views;
 
 namespace Toggl.Ross.ViewControllers
 {
     public class DurationChangeViewController : UIViewController
     {
+        private readonly TimeEntryModel model;
+        private DurationView durationView;
+
         public DurationChangeViewController (TimeEntryModel model)
         {
+            this.model = model;
+        }
+
+        public override void LoadView ()
+        {
+            View = new UIImageView () {
+                BackgroundColor = Color.Black,
+            };
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+
+            // Configure navigation item
+            NavigationItem.TitleView = durationView = new DurationView ();
+            durationView.SizeToFit ();
+
+            NavigationItem.RightBarButtonItem = new UIBarButtonItem (
+                model == null ? "DurationAdd".Tr () : "DurationSet".Tr (),
+                UIBarButtonItemStyle.Plain,
+                OnNavigationBarRightClicked
+            ).Apply (Style.NavLabelButton);
+        }
+
+        public override void ViewDidAppear (bool animated)
+        {
+            base.ViewDidAppear (animated);
+            durationView.BecomeFirstResponder ();
+        }
+
+        public override void ViewWillDisappear (bool animated)
+        {
+            base.ViewWillDisappear (animated);
+            durationView.ResignFirstResponder ();
+        }
+
+        private void OnNavigationBarRightClicked (object sender, EventArgs args)
+        {
+            // TODO: Check that the user has changed the duration
+
+            var duration = TimeSpan.Zero; // TODO
+
+            if (model == null) {
+                var m = TimeEntryModel.CreateFinished (duration);
+                var controller = new EditTimeEntryViewController (m);
+
+                // Replace self with edit controller on the stack
+                var vcs = NavigationController.ViewControllers;
+                vcs [vcs.Length - 1] = controller;
+                NavigationController.SetViewControllers (vcs, true);
+            } else {
+                model.SetDuration (duration);
+                NavigationController.PopViewControllerAnimated (true);
+            }
         }
     }
 }

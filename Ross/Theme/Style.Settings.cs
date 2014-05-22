3a3ef8ca35c2a46d70c9b0bec585f3e6379c1c08
Mart Roosmaa <mart@toggl.com>
2014-05-22 16:43:20 +0300

    Implemented settings view. Closes #173.

---
+++ Ross/Theme/Style.Settings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MonoTouch.UIKit;
+
+namespace Toggl.Ross.Theme
+{
+    public static partial class Style
+    {
+        public static class Settings
+        {
+            public static void RowBackground<T> (T v)
+                where T : UIView
+            {
+                v.BackgroundColor = Color.White;
+            }
+
+            public static void Separator<T> (T v)
+                where T : UIView
+            {
+                v.BackgroundColor = Color.Gray.ColorWithAlpha (0.1f);
+            }
+
+            public static void SettingLabel (UILabel v)
+            {
+                v.Font = UIFont.FromName ("HelveticaNeue", 17f);
+                v.TextAlignment = UITextAlignment.Left;
+                v.TextColor = Color.Black;
+            }
+
+            public static void DescriptionLabel (UILabel v)
+            {
+                v.Font = UIFont.FromName ("HelveticaNeue", 13f);
+                v.Lines = 3;
+                v.TextAlignment = UITextAlignment.Left;
+                v.TextColor = Color.Gray;
+            }
+        }
+    }
+}
--- Ross/ViewControllers/SettingsViewController.cs
+++ Ross/ViewControllers/SettingsViewController.cs
@@ -1,19 +1,180 @@
 ﻿using System;
+using System.Collections.Generic;
+using Cirrious.FluentLayouts.Touch;
 using MonoTouch.UIKit;
+using Toggl.Phoebe;
+using Toggl.Phoebe.Data;
+using XPlatUtils;
+using Toggl.Ross.Data;
 using Toggl.Ross.Theme;
+using Toggl.Ross.Views;
 
 namespace Toggl.Ross.ViewControllers
 {
     public class SettingsViewController : UIViewController
     {
+        private LabelSwitchView askProjectView;
+        private LabelSwitchView mobileTagView;
+        private Subscription<SettingChangedMessage> subscriptionSettingChanged;
+        private bool isResuming;
+
         public SettingsViewController ()
         {
             Title = "SettingsTitle".Tr ();
+            EdgesForExtendedLayout = UIRectEdge.None;
+        }
+
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing) {
+                if (subscriptionSettingChanged != null) {
+                    var bus = ServiceContainer.Resolve<MessageBus> ();
+                    bus.Unsubscribe (subscriptionSettingChanged);
+                    subscriptionSettingChanged = null;
+                }
+            }
+
+            base.Dispose (disposing);
         }
 
         public override void LoadView ()
         {
             View = new UIView ().Apply (Style.Screen);
+
+            Add (new SeparatorView ().Apply (Style.Settings.Separator));
+            Add (askProjectView = new LabelSwitchView () {
+                TranslatesAutoresizingMaskIntoConstraints = false,
+            }.Apply (Style.Settings.RowBackground).Apply (BindAskProjectView));
+            askProjectView.Label.Apply (Style.Settings.SettingLabel);
+            askProjectView.Label.Text = "SettingsAskProject".Tr ();
+            askProjectView.Switch.ValueChanged += OnAskProjectViewValueChanged;
+
+            Add (new SeparatorView ().Apply (Style.Settings.Separator));
+            Add (new UILabel () {
+                Text = "SettingsAskProjectDesc".Tr (),
+                TranslatesAutoresizingMaskIntoConstraints = false,
+            }.Apply (Style.Settings.DescriptionLabel));
+
+            Add (new SeparatorView ().Apply (Style.Settings.Separator));
+            Add (mobileTagView = new LabelSwitchView () {
+                TranslatesAutoresizingMaskIntoConstraints = false,
+            }.Apply (Style.Settings.RowBackground).Apply (BindMobileTagView));
+            mobileTagView.Label.Apply (Style.Settings.SettingLabel);
+            mobileTagView.Label.Text = "SettingsMobileTag".Tr ();
+            mobileTagView.Switch.ValueChanged += OnMobileTagViewValueChanged;
+
+            Add (new SeparatorView ().Apply (Style.Settings.Separator));
+            Add (new UILabel () {
+                Text = "SettingsMobileTagDesc".Tr (),
+                TranslatesAutoresizingMaskIntoConstraints = false,
+            }.Apply (Style.Settings.DescriptionLabel));
+
+            View.AddConstraints (MakeConstraints (View));
+        }
+
+        private void BindAskProjectView (LabelSwitchView v)
+        {
+            // TODO
+        }
+
+        private void BindMobileTagView (LabelSwitchView v)
+        {
+            v.Switch.On = SettingsStore.UseDefaultTag;
+        }
+
+        private void Rebind ()
+        {
+            askProjectView.Apply (BindAskProjectView);
+            mobileTagView.Apply (BindMobileTagView);
+        }
+
+        private void OnAskProjectViewValueChanged (object sender, EventArgs e)
+        {
+            // TODO
+        }
+
+        private void OnMobileTagViewValueChanged (object sender, EventArgs e)
+        {
+            SettingsStore.UseDefaultTag = mobileTagView.Switch.On;
+        }
+
+        public override void ViewWillAppear (bool animated)
+        {
+            base.ViewWillAppear (animated);
+
+            if (subscriptionSettingChanged == null) {
+                var bus = ServiceContainer.Resolve<MessageBus> ();
+                subscriptionSettingChanged = bus.Subscribe<SettingChangedMessage> (OnSettingChanged);
+            }
+
+            if (isResuming) {
+                Rebind ();
+            }
+
+            isResuming = true;
+        }
+
+        public override void ViewWillDisappear (bool animated)
+        {
+            base.ViewWillDisappear (animated);
+
+            if (subscriptionSettingChanged != null) {
+                var bus = ServiceContainer.Resolve<MessageBus> ();
+                bus.Unsubscribe (subscriptionSettingChanged);
+                subscriptionSettingChanged = null;
+            }
+        }
+
+        private void OnSettingChanged (SettingChangedMessage msg)
+        {
+            Rebind ();
+        }
+
+        private SettingsStore SettingsStore {
+            get { return ServiceContainer.Resolve<SettingsStore> (); }
+        }
+
+        private static IEnumerable<FluentLayout> MakeConstraints (UIView container)
+        {
+            UIView prev = null;
+
+            foreach (var view in container.Subviews) {
+                var topMargin = 0f;
+                var horizMargin = 0f;
+
+                if (view is UILabel) {
+                    topMargin = 7f;
+                    horizMargin = 15f;
+                } else if (view is SeparatorView && !(prev is LabelSwitchView)) {
+                    topMargin = 20f;
+                    horizMargin = 0f;
+                }
+
+                if (prev == null) {
+                    yield return view.AtTopOf (container, topMargin);
+                } else {
+                    yield return view.Below (prev, topMargin);
+                }
+
+                yield return view.AtLeftOf (container, horizMargin);
+                yield return view.AtRightOf (container, horizMargin);
+
+                if (view is LabelSwitchView) {
+                    yield return view.Height ().EqualTo (42f);
+                } else if (view is SeparatorView) {
+                    yield return view.Height ().EqualTo (1f);
+                }
+
+                prev = view;
+            }
+        }
+
+        private class SeparatorView : UIView
+        {
+            public SeparatorView ()
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false;
+            }
         }
     }
 }

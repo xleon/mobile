2a8324ec1585fbd5ecfdcd62d8fe7efb4161d2cc
Mart Roosmaa <mart@toggl.com>
2014-06-14 10:38:55 +0300

    Add empty model test fixtures.

---
+++ Tests/Data/Models/ModelTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Toggl.Phoebe.Data.NewModels;
+
+namespace Toggl.Phoebe.Tests.Data.Models
+{
+    public abstract class ModelTest<T> : Test
+        where T : IModel
+    {
+    }
+}
---
+++ Tests/Data/Models/TimeEntryTagModelTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using NUnit.Framework;
+using Toggl.Phoebe.Data.NewModels;
+
+namespace Toggl.Phoebe.Tests.Data.Models
+{
+    [TestFixture]
+    public class TimeEntryTagModelTest : ModelTest<TimeEntryTagModel>
+    {
+    }
+}

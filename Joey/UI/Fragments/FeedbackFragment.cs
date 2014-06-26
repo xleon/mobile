e7cd531d853746624c27589be10547517d4e3f21
taavirehemagi <taavirehemagi@gmail.com>
2014-06-26 16:04:29 +0300

    Make mood buttons responsive

--- Joey/UI/Fragments/FeedbackFragment.cs
+++ Joey/UI/Fragments/FeedbackFragment.cs
@@ -17,12 +17,68 @@ namespace Toggl.Joey.UI.Fragments
 {
     public class FeedbackFragment : Fragment
     {
+
+        public ImageButton FeedbackPositiveButton { get; private set;}
+        public ImageButton FeedbackNeutralButton { get; private set;}
+        public ImageButton FeedbackNegativeButton { get; private set;}
+        public Button SubmitFeedbackButton { get; private set; }
+        public EditText FeedbackMessageText { get; private set; }
+        public int UserMood { get; private set; }
+        public String UserMessage { get; private set; }
+
         public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             var view = inflater.Inflate (Resource.Layout.FeedbackFragment, container, false);
+
+            FeedbackPositiveButton = view.FindViewById<ImageButton> (Resource.Id.FeedbackPositiveButton);
+            FeedbackNeutralButton = view.FindViewById<ImageButton> (Resource.Id.FeedbackNeutralButton);
+            FeedbackNegativeButton = view.FindViewById<ImageButton> (Resource.Id.FeedbackNegativeButton);
+
+            FeedbackPositiveButton.Click  += (sender, e) => OnMoodButtonClick(1);
+            FeedbackNeutralButton.Click  += (sender, e) => OnMoodButtonClick(2);
+            FeedbackNegativeButton.Click  += (sender, e) => OnMoodButtonClick(3);
+
+            FeedbackMessageText = view.FindViewById<EditText> (Resource.Id.FeedbackMessageText);
+
+            SubmitFeedbackButton = view.FindViewById<Button> (Resource.Id.SendFeedbackButton);
+            SubmitFeedbackButton.Click += OnSendClick;
+
             return view;
         }
 
+        public override void OnResume()
+        {
+            OnMoodButtonClick(UserMood);
+            base.OnResume ();
+        }
+
+        void OnSendClick (object sender, EventArgs e) 
+        {
+            //Collect feedback message text, and users mood (alert when either is missing)
+            //If user submits positive message, ask if they want to insert it to app store (google play store??) and if so then copy to clipboard.
+            //When succesfully sent, reset the form and navigate away, also display toast that it succeeded.
+            
+        }
+
+        void OnMoodButtonClick (int mood)
+        {
+            UserMood = mood;
+            ResetMoodButtonImages ();
+            if (mood  == 1) {
+                FeedbackPositiveButton.SetImageResource(Resource.Drawable.IcFeedbackPositiveActive);
+            } else if (mood == 2) {
+                FeedbackNeutralButton.SetImageResource(Resource.Drawable.IcFeedbackNeutralActive);
+            } else if (mood == 3) {
+                FeedbackNegativeButton.SetImageResource(Resource.Drawable.IcFeedbackNegativeActive);
+            }
+        }
+
+        void ResetMoodButtonImages()
+        {
+            FeedbackPositiveButton.SetImageResource(Resource.Drawable.IcFeedbackPositive);
+            FeedbackNeutralButton.SetImageResource(Resource.Drawable.IcFeedbackNeutral);
+            FeedbackNegativeButton.SetImageResource(Resource.Drawable.IcFeedbackNegative);
+        }
     }
 }
 

b1d13c2a31007b3b1adb8487cf41d38c3d58f6d8
taavirehemagi <taavirehemagi@gmail.com>
2014-06-27 15:13:28 +0300

    Form validation, fake send

--- Joey/UI/Fragments/FeedbackFragment.cs
+++ Joey/UI/Fragments/FeedbackFragment.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Linq;
 using System.Text;
 
@@ -22,9 +23,9 @@ namespace Toggl.Joey.UI.Fragments
         public ImageButton FeedbackNeutralButton { get; private set;}
         public ImageButton FeedbackNegativeButton { get; private set;}
         public Button SubmitFeedbackButton { get; private set; }
-        public EditText FeedbackMessageText { get; private set; }
-        public int UserMood { get; private set; }
-        public String UserMessage { get; private set; }
+        public EditText FeedbackMessageEditText { get; private set; }
+        public int FeedbackMood { get; private set; }
+        public String FeedbackMessage { get; private set; }
 
         public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
@@ -34,11 +35,11 @@ namespace Toggl.Joey.UI.Fragments
             FeedbackNeutralButton = view.FindViewById<ImageButton> (Resource.Id.FeedbackNeutralButton);
             FeedbackNegativeButton = view.FindViewById<ImageButton> (Resource.Id.FeedbackNegativeButton);
 
-            FeedbackPositiveButton.Click  += (sender, e) => OnMoodButtonClick(1);
-            FeedbackNeutralButton.Click  += (sender, e) => OnMoodButtonClick(2);
-            FeedbackNegativeButton.Click  += (sender, e) => OnMoodButtonClick(3);
+            FeedbackPositiveButton.Click  += (sender, e) => SetMood(1);
+            FeedbackNeutralButton.Click  += (sender, e) => SetMood(2);
+            FeedbackNegativeButton.Click  += (sender, e) => SetMood(3);
 
-            FeedbackMessageText = view.FindViewById<EditText> (Resource.Id.FeedbackMessageText);
+            FeedbackMessageEditText = view.FindViewById<EditText> (Resource.Id.FeedbackMessageText);
 
             SubmitFeedbackButton = view.FindViewById<Button> (Resource.Id.SendFeedbackButton);
             SubmitFeedbackButton.Click += OnSendClick;
@@ -48,7 +49,7 @@ namespace Toggl.Joey.UI.Fragments
 
         public override void OnResume()
         {
-            OnMoodButtonClick(UserMood);
+            SetMood(FeedbackMood);
             base.OnResume ();
         }
 
@@ -57,12 +58,26 @@ namespace Toggl.Joey.UI.Fragments
             //Collect feedback message text, and users mood (alert when either is missing)
             //If user submits positive message, ask if they want to insert it to app store (google play store??) and if so then copy to clipboard.
             //When succesfully sent, reset the form and navigate away, also display toast that it succeeded.
-            
+            FeedbackMessage = FeedbackMessageEditText.Text;
+
+            if( FeedbackMessage.Length > 0 && FeedbackMood > 0 ) //valid
+                SendFeedbackData(FeedbackMessage, FeedbackMood);
+            else if ( FeedbackMessage.Length == 0)
+                FormNotValidAlert(1);
+            else if (FeedbackMood == 0)
+                FormNotValidAlert(2);
         }
 
-        void OnMoodButtonClick (int mood)
+        private bool prevSendResult;
+        private async Task<bool> SendFeedbackData ( string feedback, int mood ) {
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            prevSendResult = !prevSendResult;
+            return prevSendResult;
+        }
+
+        void SetMood (int mood)
         {
-            UserMood = mood;
+            FeedbackMood = mood;
             ResetMoodButtonImages ();
             if (mood  == 1) {
                 FeedbackPositiveButton.SetImageResource(Resource.Drawable.IcFeedbackPositiveActive);
@@ -78,6 +93,20 @@ namespace Toggl.Joey.UI.Fragments
             FeedbackPositiveButton.SetImageResource(Resource.Drawable.IcFeedbackPositive);
             FeedbackNeutralButton.SetImageResource(Resource.Drawable.IcFeedbackNeutral);
             FeedbackNegativeButton.SetImageResource(Resource.Drawable.IcFeedbackNegative);
+        }
+
+        void FormNotValidAlert(int type)
+        {
+            new AlertDialog.Builder (Activity)
+                .SetTitle ("Warning")
+                .SetMessage ("Please insert a message!")
+                .SetCancelable (false)
+                .SetPositiveButton ("OK", OnOkClicked)
+                .Show ();
+        }
+
+        private void OnOkClicked (object sender, DialogClickEventArgs e)
+        {
         }
     }
 }

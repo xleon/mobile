7975e9d71c13d6aa7c29b0b0ea5e48b42452ae62
Mart Roosmaa <mart@toggl.com>
2014-08-04 14:56:58 +0300

    Add empty sync status view controller.

---
+++ Ross/ViewControllers/SyncStatusViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.UIKit;
+
+namespace Toggl.Ross.ViewControllers
+{
+    public abstract class SyncStatusViewController : UIViewController
+    {
+        private readonly UIViewController contentViewController;
+
+        protected SyncStatusViewController (UIViewController viewController)
+        {
+            if (viewController == null)
+                throw new ArgumentNullException ("viewController");
+
+            contentViewController = viewController;
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+
+            // Add content view controller:
+            AddChildViewController (contentViewController);
+            contentViewController.View.Frame = new RectangleF (PointF.Empty, View.Frame.Size);
+            View.AddSubview (contentViewController.View);
+            contentViewController.DidMoveToParentViewController (this);
+        }
+
+        public override UINavigationItem NavigationItem {
+            get { return contentViewController.NavigationItem; }
+        }
+    }
+}

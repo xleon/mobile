bd43c7784a019eeec7a3e6193a57fca8b05f0798
Mart Roosmaa <mart@toggl.com>
2014-08-27 17:51:09 +0300

    Refactor LogStore to allow compressed log fetching.

--- Phoebe/LogStore.cs
+++ Phoebe/LogStore.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Toggl.Phoebe
 {
@@ -16,11 +18,28 @@ namespace Toggl.Phoebe
         private readonly object syncRoot = new Object ();
         private readonly Queue<string> writeQueue = new Queue<string> ();
         private readonly string logDir;
-        private bool isWriting;
+        private bool isProcessing;
+        private TaskCompletionSource<byte[]> compressionTcs;
 
         public LogStore ()
         {
             logDir = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
+        }
+
+        public Task<byte[]> Compress ()
+        {
+            Task<byte[]> ret;
+
+            lock (syncRoot) {
+                if (compressionTcs == null) {
+                    compressionTcs = new TaskCompletionSource<byte[]> ();
+                }
+                ret = compressionTcs.Task;
+            }
+
+            EnsureProcessing ();
+
+            return ret;
         }
 
         public void Record (Logger.Level level, string tag, string message, Exception exc)
@@ -45,101 +64,156 @@ namespace Toggl.Phoebe
                 writeQueue.Enqueue (data);
             }
 
-            EnsureWriting ();
+            EnsureProcessing ();
         }
 
-        private void EnsureWriting ()
+        private void EnsureProcessing ()
         {
             lock (syncRoot) {
-                if (isWriting)
+                if (isProcessing)
                     return;
 
-                isWriting = true;
+                isProcessing = true;
             }
 
-            ThreadPool.QueueUserWorkItem (delegate {
-                // Check if we need to trim the log file:
-                var logFile = new FileInfo (Path.Combine (logDir, FileName));
-                if (logFile.Exists && logFile.Length >= FileTrimThresholdSize) {
-                    var tmpFile = new FileInfo (Path.Combine (logDir, FileTempName));
-                    try {
-                        if (tmpFile.Exists)
-                            tmpFile.Delete ();
-                        File.Move (logFile.FullName, tmpFile.FullName);
-                        logFile.Refresh ();
-                        tmpFile.Refresh ();
+            ThreadPool.QueueUserWorkItem (Process);
+        }
 
-                        // Copy data over to new file
-                        using (var tmpReader = new StreamReader (tmpFile.FullName, Encoding.UTF8))
-                        using (var logWriter = new StreamWriter (logFile.FullName, false, Encoding.UTF8)) {
-                            // Skip to where we can start copying
-                            tmpReader.BaseStream.Seek (-FileTrimTargetSize, SeekOrigin.End);
-                            tmpReader.DiscardBufferedData ();
-                            tmpReader.ReadLine ();
+        private void Process (object state)
+        {
+            var logFile = new FileInfo (Path.Combine (logDir, FileName));
 
-                            string line;
-                            while ((line = tmpReader.ReadLine ()) != null) {
-                                logWriter.WriteLine (line);
-                            }
-                        }
-                    } catch (SystemException ex) {
-                        Console.WriteLine ("Failed to trim log file.");
-                        Console.WriteLine (ex);
+            // Always start out by trimming the logs
+            TrimLog (logFile);
 
-                        // Make sure that the log file is deleted so we can start a new one
-                        try {
-                            logFile.Delete ();
-                        } catch (SystemException ex2) {
-                            Console.WriteLine ("Failed to clean up log file.");
-                            Console.WriteLine (ex2);
-                        }
-                    } finally {
-                        try {
-                            tmpFile.Delete ();
-                        } catch (SystemException ex) {
-                            Console.WriteLine ("Failed to clean up temporary log file.");
-                            Console.WriteLine (ex);
-                        }
+            while (true) {
+                var shouldCompress = false;
+                var shouldFlush = false;
+
+                // Determine what to do:
+                lock (syncRoot) {
+                    if (compressionTcs != null) {
+                        shouldCompress = true;
+                    } else if (writeQueue.Count > 0) {
+                        shouldFlush = true;
+                    } else {
+                        // Nothing left to do, shutdown worker thread
+                        isProcessing = false;
+                        break;
                     }
                 }
 
-                // Flush queue to log file:
+                // Do the actual work outside of lock
+                if (shouldCompress) {
+                    CompressLogs (logFile);
+                } else if (shouldFlush) {
+                    FlushLogs (logFile);
+                }
+            }
+        }
+
+        private void TrimLog (FileInfo logFile)
+        {
+            // Check if we need to trim the log file:
+            if (logFile.Exists && logFile.Length >= FileTrimThresholdSize) {
+                var tmpFile = new FileInfo (Path.Combine (logDir, FileTempName));
                 try {
-                    using (var logWriter = new StreamWriter (logFile.FullName, true, Encoding.UTF8)) {
-                        while (true) {
-                            String data;
+                    if (tmpFile.Exists)
+                        tmpFile.Delete ();
+                    File.Move (logFile.FullName, tmpFile.FullName);
+                    logFile.Refresh ();
+                    tmpFile.Refresh ();
 
-                            // Get data to write:
-                            lock (syncRoot) {
-                                if (writeQueue.Count < 1) {
-                                    isWriting = false;
-                                    return;
-                                }
+                    // Copy data over to new file
+                    using (var tmpReader = new StreamReader (tmpFile.FullName, Encoding.UTF8))
+                    using (var logWriter = new StreamWriter (logFile.FullName, false, Encoding.UTF8)) {
+                        // Skip to where we can start copying
+                        tmpReader.BaseStream.Seek (-FileTrimTargetSize, SeekOrigin.End);
+                        tmpReader.DiscardBufferedData ();
+                        tmpReader.ReadLine ();
 
-                                data = writeQueue.Dequeue ();
-                            }
-
-                            // Write data to file:
-                            try {
-                                logWriter.WriteLine (data);
-                            } catch (SystemException ex2) {
-                                // Couldn't write to log file.. nothing to do really.
-                                Console.WriteLine ("Failed to append to log file.");
-                                Console.WriteLine (ex2);
-                            }
+                        string line;
+                        while ((line = tmpReader.ReadLine ()) != null) {
+                            logWriter.WriteLine (line);
                         }
                     }
                 } catch (SystemException ex) {
-                    // Probably opening of the log file failed, clean up
-                    Console.WriteLine ("Failed to open the log file.");
+                    Console.WriteLine ("Failed to trim log file.");
                     Console.WriteLine (ex);
 
-                    lock (syncRoot) {
-                        isWriting = false;
-                        writeQueue.Clear ();
+                    // Make sure that the log file is deleted so we can start a new one
+                    try {
+                        logFile.Delete ();
+                    } catch (SystemException ex2) {
+                        Console.WriteLine ("Failed to clean up log file.");
+                        Console.WriteLine (ex2);
+                    }
+                } finally {
+                    try {
+                        tmpFile.Delete ();
+                    } catch (SystemException ex) {
+                        Console.WriteLine ("Failed to clean up temporary log file.");
+                        Console.WriteLine (ex);
                     }
                 }
-            });
+            }
+        }
+
+        private void FlushLogs (FileInfo logFile)
+        {
+            // Flush queue to log file:
+            try {
+                using (var logWriter = new StreamWriter (logFile.FullName, true, Encoding.UTF8)) {
+                    while (true) {
+                        String data;
+
+                        // Get data to write:
+                        lock (syncRoot) {
+                            if (writeQueue.Count < 1) {
+                                return;
+                            }
+
+                            data = writeQueue.Dequeue ();
+                        }
+
+                        // Write data to file:
+                        try {
+                            logWriter.WriteLine (data);
+                        } catch (SystemException ex2) {
+                            // Couldn't write to log file.. nothing to do really.
+                            Console.WriteLine ("Failed to append to log file.");
+                            Console.WriteLine (ex2);
+                        }
+                    }
+                }
+            } catch (SystemException ex) {
+                // Probably opening of the log file failed, clean up
+                Console.WriteLine ("Failed to open the log file.");
+                Console.WriteLine (ex);
+
+                lock (syncRoot) {
+                    writeQueue.Clear ();
+                }
+            }
+        }
+
+        private void CompressLogs (FileInfo logFile)
+        {
+            byte[] data = null;
+
+            using (var logStream = logFile.OpenRead ())
+            using (var buffer = new MemoryStream ())
+            using (var zipStream = new GZipStream (buffer, CompressionMode.Compress)) {
+                logStream.CopyTo (zipStream);
+                zipStream.Close ();
+
+                data = buffer.ToArray ();
+            }
+
+            lock (syncRoot) {
+                compressionTcs.SetResult (data);
+                compressionTcs = null;
+            }
         }
     }
 }

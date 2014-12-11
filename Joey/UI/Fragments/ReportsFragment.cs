c0e4a7e49a8d99d989843ffbd6520ed3cbbffa73
Mart Roosmaa <mart@toggl.com>
2014-12-11 15:05:08 +0200

    Reorganise ReportsFragment nested classes.

--- Joey/UI/Fragments/ReportsFragment.cs
+++ Joey/UI/Fragments/ReportsFragment.cs
@@ -149,7 +149,7 @@ namespace Toggl.Joey.UI.Fragments
 
         public void OnListItemClick ( object sender, AdapterView.ItemClickEventArgs args)
         {
-            var adapter = listView.Adapter as ProjectListAdapter;
+            var adapter = listView.Adapter as ReportProjectAdapter;
             if (pieChart.CurrentSlice == args.Position) {
                 pieChart.SelectSlice (-1);
                 adapter.SetFocus (-1);
@@ -161,7 +161,7 @@ namespace Toggl.Joey.UI.Fragments
 
         private void OnSliceSelect (int pos)
         {
-            var adapter = listView.Adapter as ProjectListAdapter;
+            var adapter = listView.Adapter as ReportProjectAdapter;
             adapter.SetFocus (pos);
             if (pos != -1) {
                 listView.SmoothScrollToPositionFromTop (pos, 0);
@@ -220,7 +220,7 @@ namespace Toggl.Joey.UI.Fragments
                 if (summaryReport.Activity != null) {
                     totalValue.Text = summaryReport.TotalGrand;
                     billableValue.Text = summaryReport.TotalBillale;
-                    listView.Adapter = new ProjectListAdapter ( Activity, summaryReport.Projects);
+                    listView.Adapter = new ReportProjectAdapter ( Activity, summaryReport.Projects);
                     GeneratePieChart ();
                     barChart.Reset (summaryReport);
                     IsClean = false;
@@ -261,120 +261,120 @@ namespace Toggl.Joey.UI.Fragments
             barChart.Layout (listView.Left, posY, listView.Right, posY + listView.Bottom);
             listView.Layout (listView.Left, posY, listView.Right, posY + listView.Bottom);
         }
-    }
 
-    public class ProjectListAdapter : BaseAdapter<ReportProject>
-    {
-        private List<ReportProject> dataView;
-        private View colorSquare;
-        private TextView projectName;
-        private TextView projectDuration;
-        private int focus = -1;
-        private Android.App.Activity context;
-
-        public ProjectListAdapter ( Android.App.Activity ctx, List<ReportProject> dataView)
+        private class ReportProjectAdapter : BaseAdapter<ReportProject>
         {
-            this.dataView = dataView;
-            context = ctx;
+            private List<ReportProject> dataView;
+            private View colorSquare;
+            private TextView projectName;
+            private TextView projectDuration;
+            private int focus = -1;
+            private Android.App.Activity context;
 
-        }
+            public ReportProjectAdapter ( Android.App.Activity ctx, List<ReportProject> dataView)
+            {
+                this.dataView = dataView;
+                context = ctx;
 
-        public override Java.Lang.Object GetItem (int position)
-        {
-            return null;
-        }
-
-        public override long GetItemId (int position)
-        {
-            return position;
-        }
-
-        #region implemented abstract members of BaseAdapter
-
-        public override ReportProject this [int index]
-        {
-            get {
-                return dataView[index];
-            }
-        }
-
-        #endregion
-
-        public override View GetView (int position, View convertView, ViewGroup parent)
-        {
-            View view = convertView;
-
-            if (convertView == null) {
-                view = context.LayoutInflater.Inflate (Resource.Layout.ReportsProjectListItem, parent, false);
-                view.Tag = new ReportViewHolder (view);
-            }
-            var holder = (ReportViewHolder)view.Tag;
-            holder.Bind (dataView [position]);
-            holder.SetFocus (focus, position); // mmm...
-
-            return view;
-        }
-
-        public void SetFocus (int selected)
-        {
-            focus = selected;
-            NotifyDataSetChanged ();
-        }
-
-        public override int Count
-        {
-            get {
-                return dataView.Count;
-            }
-        }
-    }
-
-    internal class ReportViewHolder : BindableViewHolder<ReportProject>
-    {
-        private View _root;
-
-        public View ColorSquareView  { get; private set; }
-
-        public TextView NameTextView  { get; private set; }
-
-        public TextView DurationTextView  { get; private set; }
-
-        public ReportViewHolder ( View root)  : base (root)
-        {
-            NameTextView = root.FindViewById<TextView> (Resource.Id.ProjectName).SetFont (Font.Roboto);
-
-            ColorSquareView = root.FindViewById<View> (Resource.Id.ColorSquare);
-
-            DurationTextView = root.FindViewById<TextView> (Resource.Id.ProjectDuration).SetFont (Font.Roboto);
-
-            _root = root;
-        }
-
-        protected override void Rebind ()
-        {
-            if (String.IsNullOrEmpty ( DataSource.Project)) {
-                NameTextView.SetText (Resource.String.ReportsListViewNoProject);
-            } else {
-                NameTextView.Text = DataSource.Project;
             }
 
-            DurationTextView.Text = DataSource.FormattedTotalTime;
-            var squareDrawable = new GradientDrawable ();
-            squareDrawable.SetCornerRadius (5);
-            squareDrawable.SetColor (Color.ParseColor (ProjectModel.HexColors [ DataSource.Color % ProjectModel.HexColors.Length]));
-            ColorSquareView.SetBackgroundDrawable (squareDrawable);
-        }
+            public override Java.Lang.Object GetItem (int position)
+            {
+                return null;
+            }
 
-        public void SetFocus ( int focus, int position )
-        {
-            var squareDrawable = (GradientDrawable)ColorSquareView.Background;
-            if (focus != -1) {
-                _root.Alpha = (focus == position) ? 1 : 0.5f;
-                var radius = (focus == position) ? Convert.ToSingle ( ColorSquareView.Height / 2) : 5.0f;
-                squareDrawable.SetCornerRadius ( radius);
-            } else {
-                _root.Alpha = 1;
-                squareDrawable.SetCornerRadius (5);
+            public override long GetItemId (int position)
+            {
+                return position;
+            }
+
+            #region implemented abstract members of BaseAdapter
+
+            public override ReportProject this [int index]
+            {
+                get {
+                    return dataView[index];
+                }
+            }
+
+            #endregion
+
+            public override View GetView (int position, View convertView, ViewGroup parent)
+            {
+                View view = convertView;
+
+                if (convertView == null) {
+                    view = context.LayoutInflater.Inflate (Resource.Layout.ReportsProjectListItem, parent, false);
+                    view.Tag = new ProjectViewHolder (view);
+                }
+                var holder = (ProjectViewHolder)view.Tag;
+                holder.Bind (dataView [position]);
+                holder.SetFocus (focus, position); // mmm...
+
+                return view;
+            }
+
+            public void SetFocus (int selected)
+            {
+                focus = selected;
+                NotifyDataSetChanged ();
+            }
+
+            public override int Count
+            {
+                get {
+                    return dataView.Count;
+                }
+            }
+
+            private class ProjectViewHolder : BindableViewHolder<ReportProject>
+            {
+                private View _root;
+
+                public View ColorSquareView  { get; private set; }
+
+                public TextView NameTextView  { get; private set; }
+
+                public TextView DurationTextView  { get; private set; }
+
+                public ProjectViewHolder ( View root)  : base (root)
+                {
+                    NameTextView = root.FindViewById<TextView> (Resource.Id.ProjectName).SetFont (Font.Roboto);
+
+                    ColorSquareView = root.FindViewById<View> (Resource.Id.ColorSquare);
+
+                    DurationTextView = root.FindViewById<TextView> (Resource.Id.ProjectDuration).SetFont (Font.Roboto);
+
+                    _root = root;
+                }
+
+                protected override void Rebind ()
+                {
+                    if (String.IsNullOrEmpty ( DataSource.Project)) {
+                        NameTextView.SetText (Resource.String.ReportsListViewNoProject);
+                    } else {
+                        NameTextView.Text = DataSource.Project;
+                    }
+
+                    DurationTextView.Text = DataSource.FormattedTotalTime;
+                    var squareDrawable = new GradientDrawable ();
+                    squareDrawable.SetCornerRadius (5);
+                    squareDrawable.SetColor (Color.ParseColor (ProjectModel.HexColors [ DataSource.Color % ProjectModel.HexColors.Length]));
+                    ColorSquareView.SetBackgroundDrawable (squareDrawable);
+                }
+
+                public void SetFocus ( int focus, int position )
+                {
+                    var squareDrawable = (GradientDrawable)ColorSquareView.Background;
+                    if (focus != -1) {
+                        _root.Alpha = (focus == position) ? 1 : 0.5f;
+                        var radius = (focus == position) ? Convert.ToSingle ( ColorSquareView.Height / 2) : 5.0f;
+                        squareDrawable.SetCornerRadius ( radius);
+                    } else {
+                        _root.Alpha = 1;
+                        squareDrawable.SetCornerRadius (5);
+                    }
+                }
             }
         }
     }

46cca393afefd5cf646e5be36277fa34356ac449
taavirehemagi <taavirehemagi@gmail.com>
2015-01-13 12:14:58 +0200

    Test BaseTracker class.

---
+++ Tests/Analytics/BaseTrackerTest.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using Toggl.Phoebe.Analytics;
+using Toggl.Phoebe.Data;
+using XPlatUtils;
+
+namespace Toggl.Phoebe.Tests.Analytics
+{
+    [TestFixture]
+    public class BaseTrackerTest : Test
+    {
+        private TestTracker tracker;
+
+        public override void SetUp ()
+        {
+            base.SetUp();
+            ServiceContainer.Register<ISettingsStore> (Mock.Of<ISettingsStore> (
+                        (store) => store.ExperimentId == (string)null));
+            ServiceContainer.Register<ExperimentManager> (new ExperimentManager ());
+            tracker = new TestTracker ();
+        }
+
+        [Test]
+        public void TestSendSettingsChangeEvent ()
+        {
+            try {
+                tracker.SendSettingsChangeEvent (SettingName.AskForProject);
+            } catch (Exception e) {
+                Assert.AreEqual (TestTracker.SendEventExceptionMessage, e.Message);
+                Assert.AreEqual (tracker.CurrentSendData.Label, "AskForProject");
+            }
+
+            try {
+                tracker.SendSettingsChangeEvent ((SettingName)100);
+            } catch (Exception e) {
+                Assert.AreNotEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+        }
+
+        [Test]
+        public void TestSendAccountLoginEvent ()
+        {
+            try {
+                tracker.SendAccountLoginEvent (AccountCredentials.Password);
+            } catch (Exception e) {
+                Assert.AreEqual (TestTracker.SendEventExceptionMessage, e.Message);
+                Assert.AreEqual (tracker.CurrentSendData.Label, "Password");
+            }
+
+            try {
+                tracker.SendAccountLoginEvent ((AccountCredentials)100);
+            } catch (Exception e) {
+                Assert.AreNotEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+        }
+
+        [Test]
+        public void TestSendAccountCreateEvent ()
+        {
+            try {
+                tracker.SendAccountCreateEvent (AccountCredentials.Password);
+            } catch (Exception e) {
+                Assert.AreEqual (TestTracker.SendEventExceptionMessage, e.Message);
+                Assert.AreEqual (tracker.CurrentSendData.Label, "Password");
+            }
+
+            try {
+                tracker.SendAccountCreateEvent ((AccountCredentials)100);
+            } catch (Exception e) {
+                Assert.AreNotEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+        }
+
+        [Test]
+        public void TestSendAccountLogoutEvent ()
+        {
+            try {
+                tracker.SendAccountLogoutEvent ();
+            } catch (Exception e) {
+                Assert.AreEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+        }
+
+        [Test]
+        public void TestSendTimerStartEvent ()
+        {
+            try {
+                tracker.SendTimerStartEvent (TimerStartSource.AppNew);
+            } catch (Exception e) {
+                Assert.AreEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+
+            try {
+                tracker.SendTimerStartEvent ((TimerStartSource)100);
+            } catch (Exception e) {
+                Assert.AreNotEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+        }
+
+        [Test]
+        public void TestSendTimerStopEvent ()
+        {
+            try {
+                tracker.SendTimerStopEvent (TimerStopSource.App);
+            } catch (Exception e) {
+                Assert.AreEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+
+            try {
+                tracker.SendTimerStopEvent ((TimerStopSource)100);
+            } catch (Exception e) {
+                Assert.AreNotEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+        }
+    }
+}
+
---
+++ Tests/Analytics/TestTracker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Toggl.Phoebe.Analytics;
+
+namespace Toggl.Phoebe.Tests.Analytics
+{
+    public class TestTracker : BaseTracker
+    {
+
+        public const string SendEventExceptionMessage = "SendEventCalled";
+        public const string SendTimingExceptionMessage = "SendTimingCalled";
+        public SendData CurrentSendData { get; set; }
+
+        protected override void StartNewSession()
+        {
+        }
+
+        protected override void SendTiming (long elapsedMilliseconds, string category, string variable, string label = null)
+        {
+            throw new Exception (SendTimingExceptionMessage);
+        }
+
+        protected override void SendEvent (string category, string action, string label = null, long value = 0L)
+        {
+            CurrentSendData = new SendData ();
+            CurrentSendData.Category = category;
+            CurrentSendData.Action = action;
+            CurrentSendData.Label = label;
+            throw new Exception (SendEventExceptionMessage);
+        }
+
+        protected override void SetCustomDimension (int idx, string value)
+        {
+        }
+
+        public override string CurrentScreen
+        {
+            set { }
+        }
+
+        public class SendData
+        {
+            public string Category;
+            public string Action;
+            public string Label;
+        }
+    }
+}
+

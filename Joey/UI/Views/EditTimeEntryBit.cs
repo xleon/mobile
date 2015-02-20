c80119bca6d3629201b6dfd55426defc481e370b
masterrr <masterrr1995@gmail.com>
2015-02-20 22:43:43 +0300

    time entry bit code / layout, colors (incapsulation too), bckgrnd line

---
+++ Joey/UI/Views/EditTimeEntryBit.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Toggl.Joey.UI.Views
+{
+    public class EditTimeEntryBit : LinearLayout
+    {
+        public EditTimeEntryBit (Context context) :
+            base (context)
+        {
+
+            Initialize ();
+        }
+
+        public EditTimeEntryBit (Context context, IAttributeSet attrs) :
+            base (context, attrs)
+        {
+            Initialize ();
+        }
+
+        public EditTimeEntryBit (Context context, IAttributeSet attrs, int defStyle) :
+            base (context, attrs, defStyle)
+        {
+            Initialize ();
+        }
+
+
+
+        void Initialize ()
+        {
+            LayoutInflater inflater = (LayoutInflater)Context.GetSystemService (Context.LayoutInflaterService);
+            inflater.Inflate (Resource.Layout.EditTimeEntryBit, this);
+            TextView title = (TextView)FindViewById (Resource.Id.EditTimeEntryBitTitle);
+            EditText text = (EditText)FindViewById (Resource.Id.EditTimeEntryBitText);
+
+
+            TextView v = (TextView)FindViewById (Resource.Id.client_name);
+            v.TextSize = 1.0f;
+
+            text.FocusChange += (object sender, FocusChangeEventArgs e) => {
+                title.Pressed = text.HasFocus;
+            };
+            title.Pressed = this.HasFocus;
+           
+        }
+
+        public void MakeTextSmall()
+        {
+            TextView v = (TextView)FindViewById (Resource.Id.client_name);
+            v.TextSize = 4f;
+        }
+    }
+}
+

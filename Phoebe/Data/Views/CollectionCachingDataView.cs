70acf707babe2d1d658d3197431b932b06b307f5
Angel Anton <angel.anton@monday8am.com>
2015-05-29 20:01:55 +0200

    Replace unneeded observable collection.

--- Phoebe/Data/Views/CollectionCachingDataView.cs
+++ Phoebe/Data/Views/CollectionCachingDataView.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
 
@@ -9,7 +8,7 @@ namespace Toggl.Phoebe.Data.Views
     public class CollectionCachingDataView<T> : ICollectionDataView<T>, IDisposable
     {
         private readonly ICollectionDataView<T> source;
-        private ObservableCollection<T> data;
+        private List<T> data;
 
         public CollectionCachingDataView (ICollectionDataView<T> source)
         {
@@ -17,7 +16,7 @@ namespace Toggl.Phoebe.Data.Views
                 throw new ArgumentNullException ("source");
             }
 
-            data = new ObservableCollection<T> (source.Data);
+            data = new List<T> (source.Data);
 
             this.source = source;
             this.source.CollectionChanged += OnCollectionUpdated;
@@ -30,6 +29,9 @@ namespace Toggl.Phoebe.Data.Views
             source.CollectionChanged -= OnCollectionUpdated;
             source.OnIsLoadingChanged -= OnLoading;
             source.OnHasMoreChanged -= OnHasMore;
+
+            data.Clear ();
+            data = null;
         }
 
         public ICollectionDataView<T> Source
@@ -40,7 +42,7 @@ namespace Toggl.Phoebe.Data.Views
         private void OnCollectionUpdated (object sender, NotifyCollectionChangedEventArgs e)
         {
             if (e.Action == NotifyCollectionChangedAction.Reset) {
-                data = new ObservableCollection<T> (source.Data);
+                data = new List<T> (source.Data);
             }
 
             if (e.Action == NotifyCollectionChangedAction.Add) {
@@ -73,7 +75,9 @@ namespace Toggl.Phoebe.Data.Views
             }
 
             if (e.Action == NotifyCollectionChangedAction.Move) {
-                data.Move (e.OldStartingIndex, e.NewStartingIndex);
+                var savedItem = data [e.OldStartingIndex];
+                data.RemoveAt (e.OldStartingIndex);
+                data.Insert (e.NewStartingIndex, savedItem);
             }
 
             var handler = CollectionChanged;

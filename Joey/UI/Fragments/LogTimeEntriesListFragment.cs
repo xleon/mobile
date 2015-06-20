0240de569ef007de9a3982be9e2fd495e2197deb
Angel Anton <angel.anton@monday8am.com>
2015-06-20 21:46:10 +0200

    New swipe delete class based on native libraries.

--- Joey/UI/Fragments/LogTimeEntriesListFragment.cs
+++ Joey/UI/Fragments/LogTimeEntriesListFragment.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using Android.Animation;
 using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
 using Android.OS;
 using Android.Support.V4.App;
 using Android.Support.V7.Widget;
+using Android.Support.V7.Widget.Helper;
+using Android.Util;
 using Android.Views;
 using Android.Widget;
 using Toggl.Joey.Data;
@@ -38,7 +42,6 @@ namespace Toggl.Joey.UI.Fragments
         private DividerItemDecoration dividerDecoration;
         private ShadowItemDecoration shadowDecoration;
         private ItemTouchListener itemTouchListener;
-        private SwipeDismissTouchListener swipeTouchListener;
         private RecyclerViewScrollDetector scrollListener;
 
         public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -112,10 +115,12 @@ namespace Toggl.Joey.UI.Fragments
         private void SetupRecyclerView ()
         {
             // Touch listeners.
-            //swipeTouchListener = new SwipeDismissTouchListener (recyclerView, this);
             itemTouchListener = new ItemTouchListener (recyclerView, this);
-            //recyclerView.AddOnItemTouchListener (swipeTouchListener);
             recyclerView.AddOnItemTouchListener (itemTouchListener);
+
+            var touchCallback = new RecyclerSwipeCallback (ItemTouchHelper.Up | ItemTouchHelper.Down, ItemTouchHelper.Left);
+            var touchHelper = new ItemTouchHelper (touchCallback);
+            touchHelper.AttachToRecyclerView (recyclerView);
 
             // Decorations.
             dividerDecoration = new DividerItemDecoration (Activity, DividerItemDecoration.VerticalList);
@@ -133,7 +138,6 @@ namespace Toggl.Joey.UI.Fragments
             recyclerView.RemoveOnScrollListener (scrollListener);
             recyclerView.RemoveItemDecoration (shadowDecoration);
             recyclerView.RemoveItemDecoration (dividerDecoration);
-            //recyclerView.RemoveOnItemTouchListener (swipeTouchListener);
             recyclerView.RemoveOnItemTouchListener (itemTouchListener);
 
             recyclerView.GetAdapter ().Dispose ();
@@ -141,7 +145,6 @@ namespace Toggl.Joey.UI.Fragments
             logAdapter = null;
 
             itemTouchListener.Dispose ();
-            //swipeTouchListener.Dispose ();
             dividerDecoration.Dispose ();
             shadowDecoration.Dispose ();
             scrollListener.Dispose ();
@@ -294,6 +297,116 @@ namespace Toggl.Joey.UI.Fragments
                 if (owner.UndoBarVisible) {
                     owner.RemoveItemAndHideUndoBar ();
                 }
+            }
+        }
+
+        private class RecyclerSwipeCallback : ItemTouchHelper.SimpleCallback
+        {
+            private const float minThreshold = 20;
+
+            private int leftBorderWidth;
+            private Drawable backgroundShape;
+            private Context ctx;
+            private string deleteText;
+            private Paint labelPaint;
+            private Rect rect = new Rect();
+
+            private Context Ctx
+            {
+                get {
+                    if (ctx == null) {
+                        ctx = ServiceContainer.Resolve<Context> ();
+                    }
+                    return ctx;
+                }
+            }
+
+            private string DeleteText
+            {
+                get {
+                    if (string.IsNullOrEmpty (deleteText)) {
+                        deleteText = ctx.Resources.GetString (Resource.String.SwipeDeleteQuestion);
+                    }
+                    return deleteText;
+                }
+            }
+
+            private Drawable BackgroundShape
+            {
+                get {
+                    if (backgroundShape == null) {
+                        backgroundShape = Ctx.Resources.GetDrawable (Resource.Drawable.swipe_background_shape);
+                    }
+                    return backgroundShape;
+                }
+            }
+
+            private Paint LabelPaint
+            {
+                get {
+                    if (labelPaint == null) {
+                        var labelFontSize = TypedValue.ApplyDimension (ComplexUnitType.Sp, 14, ctx.Resources.DisplayMetrics);
+                        labelPaint = new Paint {
+                            Color = Color.White,
+                            TextSize = labelFontSize,
+                            AntiAlias = true,
+                        };
+                        labelPaint.GetTextBounds (DeleteText, 0, DeleteText.Length, rect);
+                        leftBorderWidth = (int)TypedValue.ApplyDimension (ComplexUnitType.Dip, 32, ctx.Resources.DisplayMetrics);
+                    }
+                    return labelPaint;
+                }
+            }
+
+            public RecyclerSwipeCallback (IntPtr a, Android.Runtime.JniHandleOwnership b) : base (a, b)
+            {
+            }
+
+            public RecyclerSwipeCallback (int p0, int p1) : base (p0, p1)
+            {
+            }
+
+            public override bool OnMove (RecyclerView p0, RecyclerView.ViewHolder p1, RecyclerView.ViewHolder p2)
+            {
+                return false;
+            }
+
+            public override int GetSwipeDirs (RecyclerView p0, RecyclerView.ViewHolder p1)
+            {
+                var adapter = ((LogTimeEntriesAdapter)p0.GetAdapter ());
+                if (adapter.GetItemViewType (p1.AdapterPosition) == LogTimeEntriesAdapter.ViewTypeContent) {
+                    return ItemTouchHelper.Right;
+                }
+                return 0;
+            }
+
+            public override void OnSwiped (RecyclerView.ViewHolder p0, int p1)
+            {
+            }
+
+            public override float GetSwipeThreshold (RecyclerView.ViewHolder p0)
+            {
+                return minThreshold;
+            }
+
+            public override void OnChildDraw (Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, bool isCurrentlyActive)
+            {
+                var itemHeight = viewHolder.ItemView.Height;
+                BackgroundShape.SetBounds (0, (int)viewHolder.ItemView.GetY (), c.Width, (int)viewHolder.ItemView.GetY () + itemHeight);
+                BackgroundShape.Draw (c);
+                c.DrawText (DeleteText, leftBorderWidth, viewHolder.ItemView.GetY () + itemHeight /2.0f + rect.Height ()/2.0f, LabelPaint);
+
+                base.OnChildDraw (c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+
+            protected override void Dispose (bool disposing)
+            {
+                if (disposing && backgroundShape != null) {
+                    backgroundShape.Dispose ();
+                    labelPaint.Dispose ();
+                }
+
+                base.Dispose (disposing);
             }
         }
     }

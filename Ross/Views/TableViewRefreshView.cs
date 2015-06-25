a58799454c475468e402313bdc5b22412cdcfb1d
masterrr <masterrr1995@gmail.com>
2015-06-25 11:44:44 +0300

    scale UIRefreshControl by 0.8 factor

--- Ross/Views/TableViewRefreshView.cs
+++ Ross/Views/TableViewRefreshView.cs
@@ -8,10 +8,12 @@ namespace Toggl.Ross.Views
     public class TableViewRefreshView : UIRefreshControl
     {
         const int sideSize = 5;
+        const float scaleFactor = .8f;
 
         public TableViewRefreshView ()
         {
             this.Apply (Style.TableViewHeader);
+            Transform = CGAffineTransform.MakeScale (scaleFactor, scaleFactor);
         }
 
         public void AdaptToTableView (UITableView tableView)

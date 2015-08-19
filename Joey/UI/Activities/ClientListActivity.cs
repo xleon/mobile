1ff276e744d8e5d33b8975d12c232500a4a7d918
Taavi Rehemägi <taavi@Taavis-MacBook-Pro.local>
2015-08-19 12:15:15 +0300

    Initial client select list for new project screen.

---
+++ Joey/UI/Activities/ClientListActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Toggl.Joey.UI.Fragments;
+using Toggl.Phoebe.Net;
+using XPlatUtils;
+
+namespace Toggl.Joey.UI.Activities
+{
+    [Activity (Label = "ClientListActivity",
+               ScreenOrientation = ScreenOrientation.Portrait,
+               Theme = "@style/Theme.Toggl.App")]
+    public class ClientListActivity : BaseActivity
+    {
+        public static readonly string ExtraWorkspaceId = "com.toggl.timer.client_list_workspace_id";
+
+        protected override void OnCreateActivity (Bundle state)
+        {
+            base.OnCreateActivity (state);
+            SetContentView (Resource.Layout.ClientListActivity);
+
+            var user = ServiceContainer.Resolve<AuthManager> ().User;
+
+            SupportFragmentManager.BeginTransaction ()
+            .Add (Resource.Id.ClientListActivityLayout, new ClientListFragment (user.DefaultWorkspaceId))
+            .Commit ();
+        }
+
+        public static List<Guid> GetIntentWorkspaceId (Android.Content.Intent intent)
+        {
+            var extras = intent.Extras;
+            if (extras == null) {
+                var list = new List<Guid> ();
+                list.Add (Guid.Empty);
+                return list;
+            }
+
+            var extraGuidArray = extras.GetStringArray (ExtraWorkspaceId);
+            return GetGuidList (extraGuidArray);
+        }
+
+        public static List<Guid> GetGuidList (string[] ids)
+        {
+            var list = new List<Guid> ();
+            foreach (var stringGuid in ids) {
+                var guid = new Guid (stringGuid);
+                list.Add (guid);
+            }
+            return list;
+        }
+    }
+}
---
+++ Phoebe/Data/Views/WorkspaceClientsView.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toggl.Phoebe.Data.DataObjects;
+using Toggl.Phoebe.Data.Utils;
+using Toggl.Phoebe.Net;
+using XPlatUtils;
+
+namespace Toggl.Phoebe.Data.Views
+{
+    public class WorkspaceClientsView : ICollectionDataView<object>, IDisposable
+    {
+        private readonly List<WorkspaceClientsView.Client> dataObjects = new List<WorkspaceClientsView.Client> ();
+        private UserData userData;
+        private Workspace currentWorkspace;
+        private Subscription<DataChangeMessage> subscriptionDataChange;
+        private bool isLoading;
+        private bool hasMore;
+        private Guid workspaceId;
+
+        public WorkspaceClientsView (Guid workspaceId)
+        {
+            this.workspaceId = workspaceId;
+            var bus = ServiceContainer.Resolve<MessageBus> ();
+            subscriptionDataChange = bus.Subscribe<DataChangeMessage> (OnDataChange);
+
+            Reload ();
+        }
+
+        public void Dispose ()
+        {
+            if (subscriptionDataChange != null) {
+                var bus = ServiceContainer.Resolve<MessageBus> ();
+                bus.Unsubscribe (subscriptionDataChange);
+                subscriptionDataChange = null;
+            }
+        }
+
+        private void OnDataChange (DataChangeMessage msg)
+        {
+            if (msg.Data is UserData) {
+                OnDataChange ((UserData)msg.Data);
+            } else if (msg.Data is WorkspaceData) {
+                OnDataChange ((WorkspaceData)msg.Data, msg.Action);
+            } else if (msg.Data is ClientData) {
+                OnDataChange ((ClientData)msg.Data, msg.Action);
+            }
+        }
+
+        private void OnDataChange (UserData data)
+        {
+            var existingData = userData;
+
+            userData = data;
+            if (existingData == null || existingData.DefaultWorkspaceId != data.DefaultWorkspaceId) {
+                OnUpdated ();
+            }
+
+            userData = data;
+        }
+
+        private void OnDataChange (WorkspaceData data, DataAction action)
+        {
+            var existingWorkspace = currentWorkspace;
+            currentWorkspace = new Workspace (data);
+            if (existingWorkspace == null || existingWorkspace.Data.Id != data.Id) {
+                OnUpdated ();
+            }
+            currentWorkspace = new Workspace (data);
+        }
+
+        private void OnDataChange (ClientData data, DataAction action)
+        {
+            var isExcluded = action == DataAction.Delete
+                             || data.DeletedAt.HasValue;
+
+            var existingData = dataObjects.FirstOrDefault (item => data.Matches (item));
+
+            if (isExcluded) {
+                if (existingData != null) {
+                    dataObjects.Remove (existingData);
+                }
+            } else {
+                var client = new Client (data);
+
+//                if (existingData != null) {
+//                    dataObjects.UpdateData (client);
+//                } else {
+                dataObjects.Add (client);
+//                }
+            }
+        }
+
+        private void OnUpdated ()
+        {
+            DispatchCollectionEvent (CollectionEventBuilder.GetEvent (NotifyCollectionChangedAction.Reset, -1, -1));
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        private void DispatchCollectionEvent (NotifyCollectionChangedEventArgs args)
+        {
+            var handler = CollectionChanged;
+            if (handler != null) {
+                handler (this, args);
+            }
+        }
+
+        public async void Reload ()
+        {
+            if (IsLoading) {
+                return;
+            }
+
+            var bus = ServiceContainer.Resolve<MessageBus> ();
+            var shouldSubscribe = subscriptionDataChange != null;
+
+            if (subscriptionDataChange != null) {
+                bus.Unsubscribe (subscriptionDataChange);
+                subscriptionDataChange = null;
+                shouldSubscribe = true;
+            }
+
+            try {
+                var store = ServiceContainer.Resolve<IDataStore> ();
+
+                userData = ServiceContainer.Resolve<AuthManager> ().User;
+                var userId = userData != null ? userData.Id : (Guid?)null;
+
+                IsLoading = true;
+                dataObjects.Clear ();
+
+                var clientsTask = store.Table<ClientData> ()
+                                  .QueryAsync (r => r.DeletedAt == null && r.WorkspaceId == workspaceId);
+
+                await clientsTask;
+                var clients = clientsTask.Result;
+
+                foreach (var clientData in clients) {
+                    var client = new Client (clientData);
+                    dataObjects.Add (client);
+                }
+
+                SortClients (dataObjects);
+            } finally {
+                IsLoading = false;
+                UpdateCollection ();
+
+                if (shouldSubscribe) {
+                    subscriptionDataChange = bus.Subscribe<DataChangeMessage> (OnDataChange);
+                }
+            }
+        }
+
+        private static void SortClients (List<Client> data)
+        {
+            data.Sort ((a, b) => String.Compare (
+                           a.Data.Name ?? String.Empty,
+                           b.Data.Name ?? String.Empty,
+                           StringComparison.Ordinal
+                       ));
+        }
+
+        public void LoadMore () {}
+
+        private void UpdateCollection ()
+        {
+            OnUpdated ();
+        }
+
+        public IEnumerable<object> Data
+        {
+            get {
+                return dataObjects;
+            }
+        }
+
+        public int Count
+        {
+            get {
+                return dataObjects.Count;
+            }
+        }
+
+        public event EventHandler OnHasMoreChanged;
+
+        public bool HasMore
+        {
+            get {
+                return hasMore;
+            }
+            private set {
+                hasMore = value;
+                if (OnHasMoreChanged != null) {
+                    OnHasMoreChanged (this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public event EventHandler OnIsLoadingChanged;
+
+        public bool IsLoading
+        {
+            get {
+                return isLoading;
+            }
+            private set {
+                isLoading = value;
+                if (OnIsLoadingChanged != null) {
+                    OnIsLoadingChanged (this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public class Client
+        {
+            private ClientData dataObject;
+            private readonly Guid workspaceId;
+
+            public Client (ClientData dataObject)
+            {
+                this.dataObject = dataObject;
+                workspaceId = dataObject.WorkspaceId;
+            }
+
+            public Client (WorkspaceData workspaceData)
+            {
+                dataObject = null;
+                workspaceId = workspaceData.Id;
+            }
+
+            public bool IsNoClient
+            {
+                get { return dataObject == null; }
+            }
+
+            public Guid WorkspaceId
+            {
+                get { return dataObject != null ? dataObject.WorkspaceId : workspaceId; }
+            }
+
+            public ClientData Data
+            {
+                get { return dataObject; }
+                set {
+                    if (value == null) {
+                        throw new ArgumentNullException ("value");
+                    }
+
+                    if (dataObject.Id != value.Id) {
+                        throw new ArgumentException ("Cannot change Id of the project.", "value");
+                    }
+                    dataObject = value;
+                }
+            }
+        }
+
+        public class Workspace
+        {
+            private WorkspaceData dataObject;
+            private readonly List<Client> clients = new List<Client> ();
+
+            public Workspace (WorkspaceData dataObject)
+            {
+                this.dataObject = dataObject;
+                clients.Add (new Client (dataObject));  // No client row.
+            }
+
+            public WorkspaceData Data
+            {
+                get { return dataObject; }
+                set {
+                    if (value == null) {
+                        throw new ArgumentNullException ("value");
+                    }
+                    if (dataObject.Id != value.Id) {
+                        throw new ArgumentException ("Cannot change Id of the workspace.", "value");
+                    }
+                    dataObject = value;
+                }
+            }
+
+            public List<Client> Clients
+            {
+                get { return clients; }
+            }
+        }
+    }
+}

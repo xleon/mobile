033eebd34c0e536ea9aa6d21022a8775da28bc3d
Angel Anton <angel.anton@monday8am.com>
2015-08-25 13:11:36 +0200

    Added simple core-tests option to FAKE scripts.

--- .build-helpers.fsx
+++ .build-helpers.fsx
@@ -7,44 +7,18 @@ open System.IO
 open System.Linq
 
 let Exec command args =
-    let result = Shell.Exec("/Library/Frameworks/Mono.framework/Versions/Current/bin/mono --runtime=v4.0 " + command, args)
-
+    let result = Shell.Exec("mono", "--runtime=v4.0 " + command + " " + args)
     if result <> 0 then failwithf "%s exited with error %d" command result
 
 let RestorePackages packageConfigFile =
     let toolFolder = Path.Combine("", ".ci/teamcity/tools/NuGet/NuGet.exe")
-    Exec toolFolder ("restore " + packageConfigFile)
+    printfn "File: %s" packageConfigFile
+    Exec toolFolder ("restore " + packageConfigFile + " -PackagesDirectory packages")
 
 let RestoreComponents solutionFile =
       let toolFolder = Path.Combine("", ".ci/teamcity/tools/xpkg/xamarin-component.exe")
       Exec toolFolder ("restore " + solutionFile)
 
 let RunNUnitTests dllPath xmlPath =
-    Exec "/Library/Frameworks/Mono.framework/Versions/Current/bin/nunit-console4" (dllPath + " -xml=" + xmlPath)
+    Exec "packages/NUnit.Runners.2.6.4/tools/nunit-console.exe" (dllPath + " -xml=" + xmlPath)
     TeamCityHelper.sendTeamCityNUnitImport xmlPath
-
-let RunUITests appPath =
-    let testAppFolder = Path.Combine("tests", "TipCalc.UITests", "testapps")
-
-    if Directory.Exists(testAppFolder) then Directory.Delete(testAppFolder, true)
-    Directory.CreateDirectory(testAppFolder) |> ignore
-
-    let testAppPath = Path.Combine(testAppFolder, DirectoryInfo(appPath).Name)
-
-    Directory.Move(appPath, testAppPath)
-
-    RestorePackages "tests/TipCalc.UITests/TipCalc.UITests.sln"
-
-    MSBuild "tests/TipCalc.UITests/bin/Debug" "Build" [ ("Configuration", "Debug"); ("Platform", "Any CPU") ] [ "tests/TipCalc.UITests/TipCalc.UITests.sln" ] |> ignore
-
-    RunNUnitTests "tests/TipCalc.UITests/bin/Debug/TipCalc.UITests.dll" "tests/TipCalc.UITests/bin/Debug/testresults.xml"
-
-let RunTestCloudTests appFile deviceList =
-    MSBuild "tests/TipCalc.UITests/bin/Debug" "Build" [ ("Configuration", "Debug"); ("Platform", "Any CPU") ] [ "tests/TipCalc.UITests/TipCalc.UITests.sln" ] |> ignore
-
-    let testCloudToken = Environment.GetEnvironmentVariable("TestCloudApiToken")
-    let args = String.Format(@"submit ""{0}"" {1} --devices {2} --series ""master"" --locale ""en_US"" --assembly-dir ""tests/TipCalc.UITests/bin/Debug"" --nunit-xml tests/TipCalc.UITests/testapps/testresults.xml", appFile, testCloudToken, deviceList)
-
-    Exec "packages/Xamarin.UITest.0.6.1/tools/test-cloud.exe" args
-
-    TeamCityHelper.sendTeamCityNUnitImport "Apps/tests/apps/testresults.xml"
--- .build.fsx
+++ .build.fsx
@@ -9,150 +9,16 @@ open Fake.XamarinHelper
 
 Target "core-build" (fun () ->
     RestorePackages "Phoebe/packages.config"
-
-    MSBuild "Phoebe/bin/Debug" "Build" [ ("Configuration", "Debug"); ("Platform", "Any CPU") ] [ "Phoebe/Phoebe.csproj" ] |> ignore
+    MSBuild "Phoebe/bin/Desktop/Debug" "Build" [ ("Configuration", "Debug"); ("Platform", "Any CPU") ] [ "Phoebe/Phoebe.Desktop.csproj" ] |> ignore
+    MSBuild "Tests/bin/Debug" "Build" [ ("Configuration", "Debug"); ("Platform", "Any CPU") ] [ "Tests/Tests.csproj" ] |> ignore
 )
 
 Target "core-tests" (fun () ->
-    RunNUnitTests "Phoebe/bin/Debug/Phoebe.Tests.dll" "src/TipCalc/bin/Debug/testresults.xml"
-)
-
-Target "ios-build" (fun () ->
-  RestorePackages "Ross/packages.config"
-
-  RestoreComponents "Mobile.sln"
-
-    iOSBuild (fun defaults ->
-        {defaults with
-            ProjectPath = "Mobile.sln"
-            Configuration = "Debug|iPhoneSimulator"
-            Target = "Build"
-        })
-)
-
-Target "ios-adhoc" (fun () ->
-    RestorePackages "Ross/packages.config"
-
-    RestoreComponents "Mobile.sln"
-
-    iOSBuild (fun defaults ->
-        {defaults with
-            ProjectPath = "Mobile.sln"
-            Configuration = "Ad-Hoc|iPhone"
-            Target = "Build"
-        })
-
-    let appPath = Directory.EnumerateFiles(Path.Combine("Ross", "bin", "iPhone", "Ad-Hoc"), "*.ipa").First()
-
-    TeamCityHelper.PublishArtifact appPath
-)
-
-Target "ios-appstore" (fun () ->
-    RestorePackages "Ross/packages.config"
-
-    RestoreComponents "Mobile.sln"
-
-    iOSBuild (fun defaults ->
-        {defaults with
-            ProjectPath = "TipCalc.iOS.sln"
-            Configuration = "AppStore|iPhone"
-            Target = "Build"
-        })
-
-    let outputFolder = Path.Combine("Ross", "bin", "iPhone", "AppStore")
-    let appPath = Directory.EnumerateDirectories(outputFolder, "*.app").First()
-    let zipFilePath = Path.Combine(outputFolder, "Ross.iOS.zip")
-    let zipArgs = String.Format("-r -y '{0}' '{1}'", zipFilePath, appPath)
-
-    Exec "zip" zipArgs
-
-    TeamCityHelper.PublishArtifact zipFilePath
-)
-
-Target "ios-uitests" (fun () ->
-    let appPath = Directory.EnumerateDirectories(Path.Combine("Ross", "bin", "iPhoneSimulator", "Debug"), "*.app").First()
-
-    RunUITests appPath
-)
-
-Target "ios-testcloud" (fun () ->
-    RestorePackages "Ross/packages.config"
-
-    RestoreComponents "Mobile.sln"
-
-    iOSBuild (fun defaults ->
-        {defaults with
-            ProjectPath = "Mobile.sln"
-            Configuration = "Debug|iPhone"
-            Target = "Build"
-        })
-
-    let appPath = Directory.EnumerateFiles(Path.Combine("Ross", "bin", "iPhone", "Debug"), "*.ipa").First()
-
-    getBuildParam "devices" |> RunTestCloudTests appPath
-)
-
-Target "android-build" (fun () ->
-    RestorePackages "Joey/packages.config"
-
-    xsbuild "Joey/bin/Release" "Build" [ ("Configuration", "Release") ] [ "Mobile.sln" ] |> ignore
-)
-
-Target "android-package" (fun () ->
-    AndroidPackage (fun defaults ->
-        {defaults with
-            ProjectPath = "Joey/Joey.csproj"
-            Configuration = "Release"
-            OutputPath = "Joey/bin/Release"
-        })
-    |> AndroidSignAndAlign (fun defaults ->
-        {defaults with
-            KeystorePath = "keystore"
-            KeystorePassword = "pass" // TODO: don't store this in the build script for a real app!
-            KeystoreAlias = "alias"
-        })
-    |> fun file -> TeamCityHelper.PublishArtifact file.FullName
-)
-
-Target "android-uitests" (fun () ->
-    AndroidPackage (fun defaults ->
-        {defaults with
-            ProjectPath = "Joey/Joey.csproj"
-            Configuration = "Release"
-            OutputPath = "Joey/bin/Release"
-        }) |> ignore
-
-    let appPath = Directory.EnumerateFiles(Path.Combine("Joey", "bin", "Release"), "*.apk", SearchOption.AllDirectories).First()
-
-    RunUITests appPath
-)
-
-Target "android-testcloud" (fun () ->
-    AndroidPackage (fun defaults ->
-        {defaults with
-            ProjectPath = "Joey/Joey.csproj"
-            Configuration = "Release"
-            OutputPath = "Joey/bin/Release"
-        }) |> ignore
-
-    let appPath = Directory.EnumerateFiles(Path.Combine("Joey", "bin", "Release"), "*.apk", SearchOption.AllDirectories).First()
-
-    getBuildParam "devices" |> RunTestCloudTests appPath
+    RestorePackages "Tests/packages.config"
+    RunNUnitTests "Tests/bin/Debug/Tests.dll" "Tests/bin/Debug/TestResult.xml"
 )
 
 "core-build"
   ==> "core-tests"
-
-"ios-build"
-  ==> "ios-uitests"
-
-"android-build"
-  ==> "android-uitests"
-
-"android-build"
-  ==> "android-testcloud"
-
-"android-build"
-  ==> "android-package"
 
 RunTarget()

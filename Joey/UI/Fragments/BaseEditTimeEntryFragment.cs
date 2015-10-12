23569c90ab5bd031408ea5e8447081ddc5c2d6ce
taavirehemagi <taavirehemagi@gmail.com>
2015-10-12 10:42:05 +0300

    Back button was not working for edit view.

--- Joey/UI/Fragments/BaseEditTimeEntryFragment.cs
+++ Joey/UI/Fragments/BaseEditTimeEntryFragment.cs
@@ -318,7 +318,7 @@ namespace Toggl.Joey.UI.Fragments
             Toolbar.SetDisplayShowCustomEnabled (true);
             Toolbar.SetDisplayShowTitleEnabled (false);
 
-            HasOptionsMenu = false;
+            HasOptionsMenu = true;
 
             ActionFAB = view.FindViewById<StartStopFab> (Resource.Id.StartStopBtn);
             StartTimeEditText = view.FindViewById<EditText> (Resource.Id.StartTimeEditText).SetFont (Font.Roboto);

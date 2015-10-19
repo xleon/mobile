22903eb094b5eea9ff50d7491f36550de37a759d
taavirehemagi <taavirehemagi@gmail.com>
2015-10-19 14:12:06 +0200

    Initial commit for Android Wear.

---
+++ Chandler/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Support.Wearable.Views;
+using Android.Widget;
+
+namespace Chandler
+{
+    [Activity (Label = "Chandler", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate (Bundle bundle)
+        {
+            base.OnCreate (bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            var v = FindViewById<WatchViewStub> (Resource.Id.watch_view_stub);
+            v.LayoutInflated += delegate {
+
+                // Get our button from the layout resource,
+                // and attach an event to it
+                Button button = FindViewById<Button> (Resource.Id.myButton);
+
+                button.Click += delegate {
+                    var notification = new NotificationCompat.Builder (this)
+                    .SetContentTitle ("Button tapped")
+                    .SetContentText ("Button tapped " + count++ + " times!")
+                    .SetSmallIcon (Android.Resource.Drawable.StatNotifyVoicemail)
+                    .SetGroup ("group_key_demo").Build ();
+
+                    var manager = NotificationManagerCompat.From (this);
+                    manager.Notify (1, notification);
+                    button.Text = "Check Notification!";
+                };
+            };
+        }
+    }
+}
+
+
+

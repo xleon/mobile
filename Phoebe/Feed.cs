3d784b784d54eb9e3af8d74419f93a2369f763e7
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2015-11-18 09:14:02 +0100

    EventDispatcher to Feed (not tested)

---
+++ Phoebe/Feed.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Toggl.Phoebe
+{
+    public class Feed<T> : IDisposable
+    {
+        event EventHandler<T> _handler;
+        public event EventHandler Disposed;
+
+        public Feed ()
+        {
+        }
+
+        public IObservable<T> Observe ()
+        {
+            return Observable
+                .FromEventPattern<T> (
+                    addHandler: h => _handler += h,
+                    removeHandler: h => _handler -= h
+                )
+                .Select (x => x.EventArgs);
+        }
+
+        public void Push (T data)
+        {
+            if (_handler != null) {
+                _handler (this, data);
+            }
+        }
+
+        /// <summary>
+        /// This will automatically remove all observers and trigger Disposed event
+        /// </summary>
+        public void Dispose ()
+        {
+            _handler = null;
+            if (Disposed != null)
+                Disposed (this, new EventArgs ());
+            Disposed = null;
+        }
+    }
+}
+

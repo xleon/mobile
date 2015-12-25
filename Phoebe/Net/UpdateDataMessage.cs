5834cad69bf40e6f2119a89c82e3e783e1c6fccf
Angel Anton <angel.anton@monday8am.com>
2015-12-25 12:10:41 +0100

    Moved time entries sync to Sync manager temporaly.

---
+++ Phoebe/Net/UpdateDataMessage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Toggl.Phoebe.Net
+{
+    public class UpdateStartedMessage : Message
+    {
+        private readonly DateTime startDate;
+
+        public UpdateStartedMessage (ISyncManager sender, DateTime startDate) : base (sender)
+        {
+            this.startDate = startDate;
+        }
+
+        public ISyncManager SyncManager
+        {
+            get { return (ISyncManager)Sender; }
+        }
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+        }
+    }
+
+    public class UpdateFinishedMessage : Message
+    {
+        private readonly DateTime startDate;
+        private readonly DateTime endDate;
+        private readonly bool hadErrors;
+        private readonly bool hadMore;
+
+        public UpdateFinishedMessage (ISyncManager sender, DateTime startDate, DateTime endDate, bool hadMore, bool hadErrors) : base (sender)
+        {
+            this.hadErrors = hadErrors;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.hadMore = hadMore;
+        }
+
+        public ISyncManager SyncManager
+        {
+            get { return (ISyncManager)Sender; }
+        }
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+        }
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+        }
+
+        public bool HadErrors
+        {
+            get { return hadErrors; }
+        }
+
+        public bool HadMore
+        {
+            get { return hadMore; }
+        }
+    }
+}

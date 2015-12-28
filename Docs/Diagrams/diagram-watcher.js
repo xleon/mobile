6d283af59f4fe70e28f50567eafa99c94d4081f0
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2015-12-28 10:54:43 +0100

    Add unidirectional architecture diagram
    Change also mermaid files extension to .mmd

--- Docs/Diagrams/diagram-watcher.js
+++ Docs/Diagrams/diagram-watcher.js
@@ -3,8 +3,8 @@ var path = require("path");
 var exec = require('child_process').exec;
 
 fs.watch(".", function(ev, filename) {
-    if (path.extname(filename) === ".mermaid") {
-        exec("mermaid " + filename, function(error, stdout, stderr) {
+    if (path.extname(filename) === ".mmd") {
+        exec("mermaid " + filename + " -w 980", function(error, stdout, stderr) {
             console.log("Rendered: " + filename);
         });
     }

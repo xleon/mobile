84a9d66e2b0fcc7798a30611f9bd2773f48ac88b
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2015-12-30 12:01:57 +0100

    Some features and more refactoring

--- Phoebe/_Store/Store.cs
+++ Phoebe/_Store/Store.cs
@@ -1,20 +1,10 @@
 ﻿using System;
-<<<<<<< HEAD
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-=======
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reactive.Linq;
-using System.Reactive.Subjects;
-using System.Threading.Tasks;
->>>>>>> First refactor for unidirectional prototype
 using Toggl.Phoebe.Helpers;
 
 namespace Toggl.Phoebe
 {
-<<<<<<< HEAD
     public static partial class Store
     {
         static readonly IObservable<StoreResultUntyped> observable;
@@ -22,23 +12,11 @@ namespace Toggl.Phoebe
 
         static Store ()
         {
-            observable = subject
-=======
-    public static class Store
-    {
-        static readonly Subject<ActionMsg> subject = new Subject<ActionMsg> ();
-        static readonly ConcurrentDictionary<string, IList<Action<object>>> suscriptors =
-            new ConcurrentDictionary<string, IList<Action<object>>> ();
-
-        static Store ()
-        {
             subject
->>>>>>> First refactor for unidirectional prototype
             // Messages only come from Dispatcher so scheduling shouldn't be necessary
             // .Synchronize (Scheduler.Default)
             .Choose (msg => {
                 var cb = GetCallback (msg.Tag);
-<<<<<<< HEAD
                 return cb != null ? Tuple.Create (cb, msg) : null;
             })
             .SelectAsync (tup => Util.TryCatchAsync (
@@ -48,73 +26,13 @@ namespace Toggl.Phoebe
                     return StoreResultUntyped.Error (DataMsg.UNCAUGHT_ERROR, ex.Message);
             }))
             .Where (res => res.Tag != DataMsg.UNCAUGHT_ERROR);
-=======
-                return cb != null ? Tuple.Create (msg, cb) : null;
-            })
-            .SelectAsync (tup => tup.Item1.TryProcessInStore (tup.Item2))
-            .Where (msg => msg.Tag != ActionMsg.ERROR)
-            .Subscribe (msg => {
-                IList<Action<object>> cbs = null;
-                var success = suscriptors.TryGetValue (msg.Tag, out cbs);
-                if (success) {
-                    foreach (var cb in cbs) {
-                        cb (msg.Data);
-                    }
-                }
-            });
->>>>>>> First refactor for unidirectional prototype
         }
 
-        /// <summary>
-        /// Only Dispatcher must call this method
-        /// </summary>
-<<<<<<< HEAD
-        public static void Send (DataMsgUntyped msg)
-=======
-        public static void Send (ActionMsg msg)
->>>>>>> First refactor for unidirectional prototype
-        {
-            subject.OnNext (msg);
-        }
-
-<<<<<<< HEAD
         public static IObservable<StoreResult<T>> Observe<T> ()
         {
             return observable
                 .Where(res => res.Tag == typeof(T).FullName)
                 .Select(res => new StoreResult<T> (res));
-=======
-        public static void Subscribe (string tag, Action<object> callback)
-        {
-            suscriptors.AddOrUpdate (tag,
-            _ => new List<Action<object>> { callback },
-            (_, cbs) => {
-                cbs.Add (callback);
-                return cbs;
-            });
-        }
-
-        public static bool Unsubscribe (string tag, Action<object> callback)
-        {
-            IList<Action<object>> cbs = null;
-            var success = suscriptors.TryGetValue (tag, out cbs);
-            if (success) {
-                var cbs2 = cbs.Where (x => x != callback).ToList ();
-                success = cbs.Count > cbs2.Count;
-                if (success) {
-                    success = suscriptors.TryUpdate (tag, cbs2, cbs);
-                }
-            }
-            return success;
-        }
-
-        public static Func<object, Task<ActionMsg>> GetCallback (string tag)
-        {
-            switch (tag) {
-            default:
-                return null;
-            }
->>>>>>> First refactor for unidirectional prototype
         }
     }
 }

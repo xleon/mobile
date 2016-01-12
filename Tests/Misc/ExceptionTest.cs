3d44821a10bc67a01c5675e5bedc400f4ba4f59a
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-01-12 09:00:26 +0100

    Check first null in ExceptionExtensions.TraverseTree
    Also add unit test

---
+++ Tests/Misc/ExceptionTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Toggl.Phoebe;
+
+namespace Toggl.Phoebe.Tests.Misc
+{
+    public class ExceptionTest : Test
+    {
+        [Test]
+        public void TestExceptionExtensions ()
+        {
+            Exception nullException = null;
+            var normalException = new Exception ();
+            var networkException = new System.Net.WebException ();
+            var nestedNetworkException = new Exception ("", new System.Net.Sockets.SocketException ());
+            var nestedNonNetworkException = new Exception ("", new Exception ());
+
+            Assert.IsFalse (nullException.IsNetworkFailure ());
+            Assert.IsFalse (normalException.IsNetworkFailure ());
+            Assert.IsTrue (networkException.IsNetworkFailure ());
+            Assert.IsTrue (nestedNetworkException.IsNetworkFailure ());
+            Assert.IsFalse (nestedNonNetworkException.IsNetworkFailure ());
+        }
+    }
+}
+

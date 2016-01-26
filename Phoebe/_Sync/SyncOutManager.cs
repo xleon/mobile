9a4f3373d4eb11872f222d47e3777c53ae6ee1c1
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-01-26 08:32:57 +0100

    Send messages in SyncOutManager

--- Phoebe/_Sync/SyncOutManager.cs
+++ Phoebe/_Sync/SyncOutManager.cs
@@ -2,11 +2,13 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Toggl.Phoebe.Data;
 using Toggl.Phoebe.Data.DataObjects;
+using Toggl.Phoebe.Data.Json.Converters;
 using Toggl.Phoebe.Logging;
 using Toggl.Phoebe.Net;
 using XPlatUtils;
-using System.Threading.Tasks;
 
 namespace Toggl.Phoebe.Sync
 {
@@ -14,10 +16,12 @@ namespace Toggl.Phoebe.Sync
     {
         // TODO: This queue must be persistant
         static ConcurrentQueue<IDataSyncMsg> queue = new ConcurrentQueue<IDataSyncMsg> ();
-        static ITogglClient client = ServiceContainer.Resolve<ITogglClient> ();
+        // TODO: Export without accessing the database?
+        static readonly IDataStore dataStore = ServiceContainer.Resolve<IDataStore> ();
+        static readonly ITogglClient client = ServiceContainer.Resolve<ITogglClient> ();
 
         static SyncOutManager ()
-        {
+        {   
             Store.Observe <IDataSyncMsg> ().Subscribe (
                 x => x.Data.Match (y => HandleMessage (new [] { y }),
                     e => {})); // TODO: Error handling
@@ -37,7 +41,7 @@ namespace Toggl.Phoebe.Sync
         static async Task EnqueueOrSend (IDataSyncMsg newMsg)
         {
             // TODO: Check internet connection
-            // TODO: Check queue size, if it reaches a limit, empty and request full sync next time
+            // TODO: Check queue size, if it reaches a limit, empty it and request full sync next time
 
             bool alreadyQueued = false;
             try {
@@ -75,21 +79,29 @@ namespace Toggl.Phoebe.Sync
         }
 
         // TODO: Check client methods results
-        static Task<bool> SendMessage (IDataSyncMsg msg)
+        // TODO: Convert messages to JSON in groups to improve performance (same transaction)
+        static async Task<bool> SendMessage (IDataSyncMsg msg)
         {
-            throw new NotImplementedException ();
+            object res = null;
+            await dataStore.ExecuteInTransactionWithMessagesAsync(async ctx => {
+                var json = msg.RawData.Export (ctx);
 
-//            object res = null;
-//            switch (msg.Verb) {
-//            case DataVerb.Post:
-//                res = await client.Create (msg.Data);
-//            case DataVerb.Put:
-//                res = await client.Update (msg.Data);
-//            case DataVerb.Delete:
-//                res = await client.Delete (msg.Data);
-//            }
-//
-//            return res != null;
+                switch (msg.Verb) {
+                case DataVerb.Post:
+                    res = await client.Create (json);
+                    break;
+                case DataVerb.Put:
+                    res = await client.Update (json);
+                    break;
+                case DataVerb.Delete:
+                    // TODO: Why Delete doesn't return anything?
+                    await client.Delete (json);
+                    res = new object();
+                    break;
+                }
+            });
+
+            return res != null;
         }
     }
 }

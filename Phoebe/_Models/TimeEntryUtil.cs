74fa272f962046a8ed5c638a21af4c92e0ac87d6
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-01-27 09:47:27 +0100

    Read&write stored json properties with reflection

--- Phoebe/_Models/TimeEntryUtil.cs
+++ Phoebe/_Models/TimeEntryUtil.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Toggl.Phoebe.Data;
 using Toggl.Phoebe.Data.DataObjects;
-using Toggl.Phoebe.Data.Json;
 
 namespace Toggl.Phoebe.Models
 {

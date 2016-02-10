782896d81243388ecb4fd75f1306a7b82094ef87
taavirehemagi <taavirehemagi@gmail.com>
2016-02-10 14:58:42 +0200

    Redesign of intro page.

--- Joey/UI/Activities/IntroActivity.cs
+++ Joey/UI/Activities/IntroActivity.cs
@@ -28,27 +28,23 @@ namespace Toggl.Joey.UI.Activities
     {
         private const string LogTag = "IntroActivity";
 
-        private ViewPager IntroPager;
         private Button LoginButton;
         private Button StartNowButton;
-        private readonly Handler handler = new Handler ();
-        private bool IsAuthenticating;
+        private TextView LoginIntroText;
+        private TextView IntroText;
+
 
         protected override void OnCreateActivity (Bundle state)
         {
             base.OnCreateActivity (state);
 
             SetContentView (Resource.Layout.IntroLayout);
-            IntroPager = FindViewById<ViewPager> (Resource.Id.IntroPager);
-            LoginButton = FindViewById<Button> (Resource.Id.LoginButton).SetFont (Font.DINMedium);;
+            IntroText = FindViewById<TextView> (Resource.Id.IntroTitle).SetFont (Font.DINMedium);
+            LoginButton = FindViewById<Button> (Resource.Id.LoginButton).SetFont (Font.DINMedium);
+            LoginIntroText = FindViewById<TextView> (Resource.Id.IntroLoginText).SetFont (Font.DINMedium);
             StartNowButton = FindViewById<Button> (Resource.Id.StartNowButton).SetFont (Font.DINMedium);
             LoginButton.Click += LoginButtonClick;
             StartNowButton.Click += StartNowButtonClick;
-
-            var fragments = GetFragments ();
-            IntroPager.Adapter = new IntroAdapter (FragmentManager, fragments);
-            IntroPager.CurrentItem = 3;
-            SwitchPager();
         }
 
         private void LoginButtonClick (object sender, EventArgs e)
@@ -63,7 +59,6 @@ namespace Toggl.Joey.UI.Activities
 
         private async Task SetUpNoUserAccountAsync ()
         {
-            IsAuthenticating = true;
             var authManager = ServiceContainer.Resolve<AuthManager> ();
             AuthResult authRes;
             try {
@@ -73,7 +68,6 @@ namespace Toggl.Joey.UI.Activities
                 log.Info (LogTag, ex, "Failed to set up offline mode.");
                 return;
             } finally {
-                IsAuthenticating = false;
             }
 
             StartAuthActivity ();
@@ -85,7 +79,6 @@ namespace Toggl.Joey.UI.Activities
             if (authManager.IsAuthenticated) {
                 // Try to avoid flickering of buttons during activity transition by
                 // faking that we're still authenticating
-                IsAuthenticating = true;
 
                 var intent = new Intent (this, typeof (MainDrawerActivity));
                 intent.AddFlags (ActivityFlags.ClearTop);
@@ -95,76 +88,6 @@ namespace Toggl.Joey.UI.Activities
             }
 
             return false;
-        }
-
-        private void SwitchPager ()
-        {
-            var pageNumber = IntroPager.CurrentItem;
-            pageNumber++;
-
-            if (pageNumber == 4) {
-                pageNumber = 0;
-            }
-            IntroPager.SetCurrentItem (pageNumber, true);
-
-            handler.RemoveCallbacks (SwitchPager);
-            handler.PostDelayed (SwitchPager, 5000);
-        }
-
-        private List<Fragment> GetFragments ()
-        {
-            var fr = new List<Fragment> ();
-            fr.Add (IntroScreenFragment.NewInstance (Resource.String.IntroSlideTitle1));
-            fr.Add (IntroScreenFragment.NewInstance (Resource.String.IntroSlideTitle2));
-            fr.Add (IntroScreenFragment.NewInstance (Resource.String.IntroSlideTitle3));
-            fr.Add (IntroScreenFragment.NewInstance (Resource.String.IntroSlideTitle4));
-            return fr;
-        }
-
-        private class IntroAdapter : FragmentPagerAdapter
-        {
-            private readonly List<Fragment> fragments;
-
-            public IntroAdapter (FragmentManager fm, List<Fragment> fragments) : base (fm)
-            {
-                this.fragments = fragments;
-            }
-
-            public override int Count
-            {
-                get {
-                    return fragments.Count;
-                }
-            }
-
-            public override Fragment GetItem (int position)
-            {
-                return fragments[position];
-            }
-        }
-
-        private class IntroScreenFragment : Fragment
-        {
-            private TextView TitleView;
-            private readonly int titleResource;
-
-            public static IntroScreenFragment NewInstance (int title)
-            {
-                return new IntroScreenFragment (title);
-            }
-
-            public IntroScreenFragment (int title)
-            {
-                titleResource = title;
-            }
-
-            public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-            {
-                var view = inflater.Inflate (Resource.Layout.IntroScreen, container, false);
-                TitleView = view.FindViewById<TextView> (Resource.Id.IntroTitle).SetFont (Font.DINMedium);;
-                TitleView.SetText (titleResource);
-                return view;
-            }
         }
     }
 }

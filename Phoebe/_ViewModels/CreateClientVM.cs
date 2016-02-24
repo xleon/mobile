57282400d7a6bfae01ef498003763a0f19ba621d
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-02-24 20:24:47 +0100

    Integrate CreateClient and TagList view models

--- Phoebe/_ViewModels/CreateClientVM.cs
+++ Phoebe/_ViewModels/CreateClientVM.cs
@@ -10,7 +10,49 @@ using Toggl.Phoebe._Data.Models;
 using Toggl.Phoebe._Helpers;
 using Toggl.Phoebe._Reactive;
 using XPlatUtils;
+using Toggl.Phoebe._Data;
 
 namespace Toggl.Phoebe._ViewModels
 {
+    public class CreateClientViewModel : IDisposable
+    {
+        private ClientData model;
+        private TimerState timerState;
+
+        public CreateClientViewModel (TimerState timerState, Guid workspaceId)
+        {
+            
+            var workspace = timerState.Workspaces[workspaceId];
+            model = new ClientData {
+                Id = Guid.NewGuid (),
+                WorkspaceId = workspaceId,
+                WorkspaceRemoteId = workspace.RemoteId.HasValue ? workspace.RemoteId.Value : 0
+            };
+            ServiceContainer.Resolve<ITracker> ().CurrentScreen = "New Client Screen";
+        }
+
+        public void Dispose ()
+        {
+        }
+
+        public string ClientName { get; set; }
+
+        public ClientData SaveNewClient ()
+        {
+            // Save client name to make sure it doesn't change while iterating
+            var clientName = ClientName;
+            var existing = timerState.Clients.Values
+                                     .SingleOrDefault (r => r.WorkspaceId == model.WorkspaceId && r.Name == clientName);
+
+            if (existing != null) {
+                model = existing;
+            } else {
+                model.Name = clientName;
+            }
+
+            RxChain.Send (new DataMsg.ClientDataPut (model));
+            
+            return model;
+        }
+    }
 }
--- Phoebe/_ViewModels/TagListVM.cs
+++ Phoebe/_ViewModels/TagListVM.cs
@@ -10,7 +10,61 @@ using Toggl.Phoebe._Data.Models;
 using Toggl.Phoebe._Helpers;
 using Toggl.Phoebe._Reactive;
 using XPlatUtils;
+using Toggl.Phoebe.Data.Utils;
 
 namespace Toggl.Phoebe._ViewModels
 {
+    public class TagListViewModel : IDisposable
+    {
+        // This viewMode is apparently simple but
+        // it needs the code related with the update of
+        // the list. (subscription and reload of data
+        // everytime a tag is changed/created/deleted
+
+        private readonly Guid workspaceId;
+        private readonly List<Guid> previousSelectedIds;
+
+        TagListViewModel (TimerState timerState, Guid workspaceId, List<Guid> previousSelectedIds)
+        {
+            this.previousSelectedIds = previousSelectedIds;
+            this.workspaceId = workspaceId;
+            LoadTags (timerState);
+            ServiceContainer.Resolve<ITracker> ().CurrentScreen = "Select Tags";
+        }
+
+        public void Dispose ()
+        {
+            TagCollection = null;
+        }
+
+        public ObservableRangeCollection<TagData> TagCollection { get; set; }
+
+        private void LoadTags (TimerState timerState)
+        {
+            TagCollection = new ObservableRangeCollection<TagData> ();
+
+            var workspaceTags =
+                timerState.Tags.Values.Where (r => r.WorkspaceId == workspaceId).ToList ();
+
+            var currentSelectedTags =
+                timerState.Tags.Values.Where (r => previousSelectedIds.Contains (r.Id)).ToList ();
+
+            // TODO:
+            // There is an strange case, tags are created again across
+            // workspaces. To avoid display similar names
+            // on the list the diff and corrupt data, the filter is done by
+            // names. The bad point is that tags will appears unselected.
+            var diff = currentSelectedTags.Where (sTag => workspaceTags.All (wTag => sTag.Name != wTag.Name));
+
+            workspaceTags.AddRange (diff);
+
+            workspaceTags.Sort ((a, b) => {
+                var aName = a != null ? (a.Name ?? String.Empty) : String.Empty;
+                var bName = b != null ? (b.Name ?? String.Empty) : String.Empty;
+                return String.Compare (aName, bName, StringComparison.Ordinal);
+            });
+
+            TagCollection.AddRange (workspaceTags);
+        }
+    }
 }

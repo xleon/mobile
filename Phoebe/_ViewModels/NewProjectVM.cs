5923cfb5d02e7c89cf919d862c67f382646c86bc
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-02-24 15:08:23 +0100

    Adapt NewProjectViewModel

--- Phoebe/_ViewModels/NewProjectVM.cs
+++ Phoebe/_ViewModels/NewProjectVM.cs
@@ -1,25 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
-using GalaSoft.MvvmLight;
 using PropertyChanged;
 using Toggl.Phoebe.Analytics;
-using Toggl.Phoebe.Logging;
+using Toggl.Phoebe._Data;
 using Toggl.Phoebe._Data.Models;
-using Toggl.Phoebe._Helpers;
 using Toggl.Phoebe._Reactive;
 using XPlatUtils;
-using Toggl.Phoebe._Data;
 
 namespace Toggl.Phoebe._ViewModels
 {
     [ImplementPropertyChanged]
     public class NewProjectVM : IDisposable
     {
-        private TimerState timerState;
-        private WorkspaceData workspace;
-        private ProjectData model;
+        private readonly TimerState timerState;
+        private readonly WorkspaceData workspace;
+        private readonly ProjectData model;
 
         public NewProjectVM (TimerState timerState, Guid workspaceId)
         {
@@ -37,17 +33,9 @@ namespace Toggl.Phoebe._ViewModels
 
         public void Dispose ()
         {
-            model = null;
         }
 
-        public string ProjectName { get; set; }
-
-        public int ProjectColor { get; set; }
-
         public string ClientName { get; set; }
-
-        public Guid ProjectId {  get { return model.Id; } } // TODO: not good :(
-
         public void SetClient (ClientData clientData)
         {
             model.ClientId = clientData.Id;
@@ -55,21 +43,10 @@ namespace Toggl.Phoebe._ViewModels
             ClientName = clientData.Name;
         }
 
-        public SaveProjectResult SaveProjectData ()
+        public ProjectData SaveProject (string projectName, int projectColor)
         {
-            // Project name is empty
-            if (string.IsNullOrEmpty (ProjectName)) {
-                return SaveProjectResult.NameIsEmpty;
-            }
-
-            // Project name is used
-            var exists = ExistProjectWithName (ProjectName);
-            if (exists) {
-                return SaveProjectResult.NameExists;
-            }
-
-            model.Name = ProjectName;
-            model.Color = ProjectColor;
+            model.Name = projectName;
+            model.Color = projectColor;
 
             // Create an extra model for Project / User relationship
             var userData = ServiceContainer.Resolve<Net.AuthManager> ().User;
@@ -85,20 +62,13 @@ namespace Toggl.Phoebe._ViewModels
             // Save new project and relationship
             RxChain.Send (new DataMsg.ProjectDataPut (model, projectUser));
 
-            // TODO: Wait result from RxChain?
-            return SaveProjectResult.SaveOk;
+            return model;
         }
 
         private bool ExistProjectWithName (string projectName)
         {
             Guid clientId = model.ClientId;
-            return timerState.Projects.Values.Any (r => r.Name == projectName && r.ClientId == clientId);;
-        }
-
-        public enum SaveProjectResult {
-            SaveOk = 0,
-            NameIsEmpty = 1,
-            NameExists = 2
+            return timerState.Projects.Values.Any (r => r.Name == projectName && r.ClientId == clientId);
         }
     }
 }

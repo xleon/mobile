db705e7dfa2b482a626cfc5aea940f6a6b7c8743
Angel Anton <angel.anton@monday8am.com>
2016-03-09 19:42:06 +0100

    Updated iOS project to use shared projects.

--- Phoebe/ServiceContainer.cs
+++ Phoebe/ServiceContainer.cs
@@ -16,9 +16,9 @@ namespace XPlatUtils
         /// <summary>
         /// Register the specified service with an instance
         /// </summary>
-        public static void Register<T>(T service)
+        public static void Register<T> (T service)
         {
-            services[typeof(T)] = new Lazy<object>(() => service);
+            services[typeof (T)] = new Lazy<object> (() => service);
         }
 
         /// <summary>
@@ -26,50 +26,47 @@ namespace XPlatUtils
         /// </summary>
         public static void Register<T>() where T : new()
         {
-            services[typeof(T)] = new Lazy<object>(() => new T());
+            services[typeof (T)] = new Lazy<object> (() => new T());
         }
 
         /// <summary>
         /// Register the specified service with a callback to be invoked when requested
         /// </summary>
-        public static void Register<T>(Func<T> function)
+        public static void Register<T> (Func<T> function)
         {
-            services[typeof(T)] = new Lazy<object>(() => function());
+            services[typeof (T)] = new Lazy<object> (() => function());
         }
 
         /// <summary>
         /// Register the specified service with an instance
         /// </summary>
-        public static void Register(Type type, object service)
+        public static void Register (Type type, object service)
         {
-            services[type] = new Lazy<object>(() => service);
+            services[type] = new Lazy<object> (() => service);
         }
 
         /// <summary>
         /// Register the specified service with a callback to be invoked when requested
         /// </summary>
-        public static void Register(Type type, Func<object> function)
+        public static void Register (Type type, Func<object> function)
         {
-            services[type] = new Lazy<object>(function);
+            services[type] = new Lazy<object> (function);
         }
 
         /// <summary>
         /// Register the specified service with an instance that is scoped
         /// </summary>
-        public static void RegisterScoped<T>(T service)
+        public static void RegisterScoped<T> (T service)
         {
             Dictionary<Type, object> services;
-            if (scopedServices.Count == 0)
-            {
+            if (scopedServices.Count == 0) {
                 services = new Dictionary<Type, object>();
-                scopedServices.Push(services);
-            }
-            else
-            {
+                scopedServices.Push (services);
+            } else {
                 services = scopedServices.Peek();
             }
 
-            services[typeof(T)] = service;
+            services[typeof (T)] = service;
         }
 
         /// <summary>
@@ -77,22 +74,20 @@ namespace XPlatUtils
         /// </summary>
         public static T Resolve<T>()
         {
-            return (T)Resolve(typeof(T));
+            return (T)Resolve (typeof (T));
         }
 
         /// <summary>
         /// Resolves the type, throwing an exception if not found
         /// </summary>
-        public static object Resolve(Type type)
+        public static object Resolve (Type type)
         {
             //Scoped services
-            if (scopedServices.Count > 0)
-            {
+            if (scopedServices.Count > 0) {
                 var services = scopedServices.Peek();
 
                 object service;
-                if (services.TryGetValue(type, out service))
-                {
+                if (services.TryGetValue (type, out service)) {
                     return service;
                 }
             }
@@ -100,13 +95,10 @@ namespace XPlatUtils
             //Non-scoped services
             {
                 Lazy<object> service;
-                if (services.TryGetValue(type, out service))
-                {
+                if (services.TryGetValue (type, out service)) {
                     return service.Value;
-                }
-                else
-                {
-                    throw new KeyNotFoundException(string.Format("Service not found for type '{0}'", type));
+                } else {
+                    throw new KeyNotFoundException (string.Format ("Service not found for type '{0}'", type));
                 }
             }
         }
@@ -116,7 +108,7 @@ namespace XPlatUtils
         /// </summary>
         public static void AddScope()
         {
-            scopedServices.Push(new Dictionary<Type, object>());
+            scopedServices.Push (new Dictionary<Type, object>());
         }
 
         /// <summary>
@@ -124,8 +116,9 @@ namespace XPlatUtils
         /// </summary>
         public static void RemoveScope()
         {
-            if (scopedServices.Count > 0)
+            if (scopedServices.Count > 0) {
                 scopedServices.Pop();
+            }
         }
 
         /// <summary>

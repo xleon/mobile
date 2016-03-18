56f549fd75f5c58be7e9dcfbd93002991a6d7b49
amulware <amul@amulware.net>
2016-03-18 09:17:47 +0100

    added full list of secrets to replacer scripts, including iOS

--- build-android-secrets.js
+++ build-android-secrets.js
@@ -1,13 +1,9 @@
 
-// config
-var buildFileName = "Phoebe/Build.cs";
-var secrets = {
-	// "string to replace" : "secret env variable"
-	"{GMC_SENDER_ID}" : "GCM_SENDER_ID",
-	"{RAYGUN_API_KEY}" : "RAYGUN_API_KEY",
-};
-
-
 var replacer = require("./node/secret-replace.js");
 
-replacer.replaceSecrets(buildFileName, secrets);
+replacer.replaceSecrets("Phoebe/Build.cs", {
+	// "string to replace" : "secret env variable"
+	"{GMC_SENDER_ID}" : "GCM_SENDER_ID",
+	"{RAYGUN_API_KEY_ANDROID}" : "RAYGUN_API_KEY_ANDROID",
+	"{GOOGLE_ANALYTICS_ID}" : "GOOGLE_ANALYTICS_ID",
+});

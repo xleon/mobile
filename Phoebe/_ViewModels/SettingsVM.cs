951d71fcd885d103b4b9f9745e1ea39fde2a6a1d
amulware <amul@amulware.net>
2016-03-29 14:54:02 +0200

    added settings view model

---
+++ Phoebe/_ViewModels/SettingsVM.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Reactive.Linq;
+using PropertyChanged;
+using Toggl.Phoebe._Data;
+using Toggl.Phoebe._Reactive;
+
+namespace Toggl.Phoebe._ViewModels
+{
+    [ImplementPropertyChanged]
+    public class SettingsVM : IDisposable
+    {
+        private readonly IDisposable subscription;
+
+        public SettingsVM(AppState state)
+        {
+            this.subscription = StoreManager.Singleton
+                .Observe(x => x.State)
+                .StartWith(state)
+                .Subscribe(this.stateUpdated);
+        }
+
+        #region exposed UI properties
+
+        public bool ShowNotification { get; private set; }
+        public bool IdleNotification { get; private set; }
+        public bool ChooseProjectForNew { get; private set; }
+        public bool UseDefaultTag { get; private set; }
+        public bool GroupedTimeEntries { get; private set; }
+
+        #endregion
+
+        #region public methods
+
+        public void SetShowNotification(bool value)
+        {
+            setSetting(nameof(SettingsState.ShowNotification), value);
+        }
+        public void SetIdleNotification(bool value)
+        {
+            setSetting(nameof(SettingsState.IdleNotification), value);
+        }
+        public void SetChooseProjectForNew(bool value)
+        {
+            setSetting(nameof(SettingsState.ChooseProjectForNew), value);
+        }
+        public void SetUseDefaultTag(bool value)
+        {
+            setSetting(nameof(SettingsState.UseDefaultTag), value);
+        }
+        public void SetGroupedTimeEntries(bool value)
+        {
+            setSetting(nameof(SettingsState.GroupedEntries), value);
+        }
+
+        #endregion
+
+        private void stateUpdated(AppState state)
+        {
+            var settings = state.Settings;
+
+            this.ShowNotification = settings.ShowNotification;
+            this.IdleNotification = settings.IdleNotification;
+            this.ChooseProjectForNew = settings.ChooseProjectForNew;
+            this.UseDefaultTag = settings.UseDefaultTag;
+            this.GroupedTimeEntries = settings.GroupedEntries;
+        }
+
+        private static void setSetting(string propertyName, object value)
+        {
+            RxChain.Send(new DataMsg.UpdateSetting(propertyName, value));
+        }
+
+        public void Dispose()
+        {
+            this.subscription.Dispose();
+        }
+    }
+}
+

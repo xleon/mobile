abc531b02951b70488760f0b329abf4af1f26695
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-05-05 15:03:22 +0200

    iOS LoginAndAddEntry UI Test

--- Ross.UITests/Tests.cs
+++ Ross.UITests/Tests.cs
@@ -19,10 +19,37 @@ namespace Ross.UITests
             app = ConfigureApp.iOS.StartApp();
         }
 
-        [Test]
-        public void AppLaunches()
+        private void login()
         {
-            app.Repl();
+            // Tap "Log in" button
+            app.Tap(c => c.Marked("Log in"));
+
+            // Login
+            app.EnterText(c => c.Marked("Email address"), "a@a.es");
+            app.EnterText(c => c.Marked("Password"), "123456");
+            app.Tap(c => c.Marked("Log in"));
+
+        }
+
+
+        [Test]
+        public void LoginAndAddEntry()
+        {
+            login();
+
+            // Wait for timer screen and check how many elements are in the list
+            app.WaitForElement(c => c.Marked("Start"));
+            var childCount1 = app.Query(c => c.Class("UITableViewWrapperView").Child()).Count();
+
+            // After tapping the Continue button, there should be more elements in the list
+            app.Tap(c => c.Marked("Start"));
+            app.Back();
+
+            var childCount2 = app.Query(c => c.Class("UITableViewWrapperView").Child()).Count();
+            Assert.Greater(childCount2, childCount1);
+
+            // Stop the new entry
+            app.Tap(c => c.Marked("Stop"));
         }
     }
 }

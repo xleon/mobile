4927782a4e0016e708aa584e9847dc342c108603
Angel Anton <angel.anton@monday8am.com>
2016-05-09 15:21:55 -0400

    Added dedicated fragments to NoApi Feedback/Fragment views.

---
+++ Joey/UI/Fragments/FeedbackNoApiFragment.cs
@@ -0,0 +1,26 @@
+using System;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using Fragment = Android.Support.V4.App.Fragment;
+
+namespace Toggl.Joey.UI.Fragments
+{
+    public class FeedbackNoApiFragment : Fragment
+    {
+        private Button noUserRegisterButton;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.FeedbackNoApiFragment, container, false);
+            noUserRegisterButton = view.FindViewById<Button>(Resource.Id.FeedbackRegisterButton);
+            noUserRegisterButton.Click += OpenRegisterScreen;
+            return view;
+        }
+
+        private void OpenRegisterScreen(object sender, EventArgs e)
+        {
+            //((MainDrawerActivity)Activity).OpenPage(DrawerListAdapter.RegisterUserPageId);
+        }
+    }
+}

85717dbf5391bcfc401608328999b7ec047d1069
amulware <amul@amulware.net>
2016-05-09 10:31:23 +0200

    added dummy ui-test project (so far only configured for android)

---
+++ UITests/AppInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.UITest;
+
+namespace UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                       .Android
+                       .InstalledApp("com.toggl.timer")
+                       .StartApp();
+            }
+
+            return ConfigureApp
+                   .iOS
+                   .StartApp();
+
+        }
+    }
+}

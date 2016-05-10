14d2fd9289c977fa2db5843ed6a33ab528db2016
amulware <amul@amulware.net>
2016-05-10 14:21:23 +0200

    fixed android build secrets replacer

--- build-android-secrets.js
+++ build-android-secrets.js
@@ -4,6 +4,6 @@ var replacer = require("./node/secret-replace.js");
 replacer.replaceSecrets("Phoebe/Build.cs", {
 	// "string to replace" : "secret env variable"
 	"{GMC_SENDER_ID}" : "GCM_SENDER_ID",
-	"{RAYGUN_API_KEY_ANDROID}" : "RAYGUN_API_KEY_ANDROID",
+	"{XAMARIN_INSIGHTS_API_KEY_ANDROID}" : "XAMARIN_INSIGHTS_API_KEY_ANDROID",
 	"{GOOGLE_ANALYTICS_ID}" : "GOOGLE_ANALYTICS_ID",
 });

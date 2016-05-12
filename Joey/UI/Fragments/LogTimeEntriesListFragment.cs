b361d8a74dde375d802d725821f73770a51e38d3
Angel Anton <angel.anton@monday8am.com>
2016-05-11 23:11:26 -0400

    LogTimeEntriesList updated with new welcome view.

--- Joey/UI/Fragments/LogTimeEntriesListFragment.cs
+++ Joey/UI/Fragments/LogTimeEntriesListFragment.cs
@@ -38,8 +38,7 @@ namespace Toggl.Joey.UI.Fragments
         private View experimentEmptyView;
         private LogTimeEntriesAdapter logAdapter;
         private TimerComponent timerComponent;
-        private View welcomeLayout;
-        private TextView noItemsMessage;
+        private View welcomeView;
 
         // Recycler setup
         private DividerItemDecoration dividerDecoration;
@@ -70,13 +69,12 @@ namespace Toggl.Joey.UI.Fragments
             coordinatorLayout = view.FindViewById<CoordinatorLayout> (Resource.Id.logCoordinatorLayout);
             experimentEmptyView = view.FindViewById<View> (Resource.Id.ExperimentEmptyMessageView);
             emptyMessageView = view.FindViewById<View> (Resource.Id.EmptyMessageView);
-            welcomeLayout = view.FindViewById<View> (Resource.Id.WelcomeLayout);
+            welcomeView = view.FindViewById<View> (Resource.Id.WelcomeLayout);
 
             view.FindViewById<TextView>(Resource.Id.welcomeHelloTextView).SetFont(Font.TektonPro);
             view.FindViewById<TextView>(Resource.Id.welcomeSignInTextView).SetFont(Font.TektonPro);
             view.FindViewById<TextView>(Resource.Id.welcomeStartTextView).SetFont(Font.TektonPro);
 
-            noItemsMessage = view.FindViewById<TextView> (Resource.Id.EmptyTitleTextView);
             recyclerView = view.FindViewById<RecyclerView> (Resource.Id.LogRecyclerView);
             recyclerView.SetLayoutManager(new LinearLayoutManager(Activity));
             swipeLayout = view.FindViewById<SwipeRefreshLayout> (Resource.Id.LogSwipeContainer);
@@ -274,9 +272,11 @@ namespace Toggl.Joey.UI.Fragments
         private void SetFooterState()
         {
             var info = ViewModel.LoadInfo;
-            if (ViewModel.IsNoUserMode || (!info.HasMore && !info.HadErrors))
+
+            if (!info.HasMore && !info.HadErrors)
             {
                 logAdapter.SetFooterState(RecyclerCollectionDataAdapter<IHolder>.RecyclerLoadState.Finished);
+                SetCollectionState();
             }
             else if (info.HasMore && !info.HadErrors)
             {
@@ -312,9 +312,8 @@ namespace Toggl.Joey.UI.Fragments
             }
 
             // According to settings, show welcome message or no.
-            //welcomeMessage.Visibility = isWelcome ? ViewStates.Visible : ViewStates.Gone;
-            noItemsMessage.Visibility = isWelcome ? ViewStates.Gone : ViewStates.Visible;
-            emptyView.Visibility = hasItems ? ViewStates.Gone : ViewStates.Visible;
+            welcomeView.Visibility = isWelcome ? ViewStates.Visible : ViewStates.Gone;
+            emptyView.Visibility = (!isWelcome && !hasItems) ? ViewStates.Visible : ViewStates.Gone;
             recyclerView.Visibility = hasItems ? ViewStates.Visible : ViewStates.Gone;
         }
         #endregion

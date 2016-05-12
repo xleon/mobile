8bb915673a449f3b315983ca756acd63d2efe8bf
Angel Anton <angel.anton@monday8am.com>
2016-05-12 17:06:12 -0400

    Migration detection and fragment added.

--- Joey/UI/Fragments/FeedbackNoApiFragment.cs
+++ Joey/UI/Fragments/FeedbackNoApiFragment.cs
@@ -12,6 +12,14 @@ namespace Toggl.Joey.UI.Fragments
     {
         private Button noUserRegisterButton;
 
+        public FeedbackNoApiFragment()
+        {
+        }
+
+        public FeedbackNoApiFragment(IntPtr jref, Android.Runtime.JniHandleOwnership xfer) : base(jref, xfer)
+        {
+        }
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             var view = inflater.Inflate(Resource.Layout.FeedbackNoApiFragment, container, false);
